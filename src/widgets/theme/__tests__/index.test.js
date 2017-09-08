569831e9ff1e2dc5fc1fca25e02ef6c7d9e2eac5
zenjava <138962816@qq.com>
2017-09-08 13:28:03 +0800

    修改：   * theme测试用例。

---
+++ src/widgets/theme/__tests__/index.test.js
@@ -0,0 +1,31 @@
+//@flow
+
+import * as React from 'react';
+import chai from 'chai';
+import Theme from '../';
+
+import renderer from 'react-test-renderer';
+import PropTypes from 'prop-types';
+
+const { expect: exp, } = chai;
+
+describe('Theme', () => {
+
+  it('Theme', () => {
+
+    class TestComponent extends React.Component<any> {
+      render () {
+        return <div>{JSON.stringify(this.context.config)}{this.props.children}</div>;
+      }
+    }
+
+    TestComponent.contextTypes = {
+      config: PropTypes.object,
+    };
+    const config = { ligx: { value: '正念', }, };
+    const cmp = renderer.create(<Theme config={config}><TestComponent>hello everyone</TestComponent></Theme>);
+    expect(cmp).toMatchSnapshot();
+  });
+
+
+});
