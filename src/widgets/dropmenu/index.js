dcd68212951edcb9fcba4e77f95dd42554b443e2
zenjava <138962816@qq.com>
2017-09-11 15:03:51 +0800

    修改：   * 实现下来菜单。

--- src/widgets/dropmenu/index.js
+++ src/widgets/dropmenu/index.js
@@ -1,24 +1,72 @@
-
+/**
+ * 下拉菜单
+ * by ligx
+ * @flow
+ */
 import * as React from 'react';
+import ReactDom from 'react-dom';
 import styled from 'styled-components';
+import Trigger from '../trigger';
+import Theme from '../theme';
+import * as Widget from '../consts/Widget';
 
-type MenuProps = {
+type DropMenuProps = {
+  menus: React.Node,
   children: React.Node
 };
-
 const MenuContainer = styled.div`
    background-color: #fff;
    box-shadow: 0 1px 6px rgba(0,0,0,.2);
    border-radius: 4px;
    box-sizing: border-box;
 `;
+type DropMenuState = {
+  trigerWidth: string;
+};
 
-class Menu extends React.Component<MenuProps> {
+class DropMenu extends React.Component<DropMenuProps, DropMenuState> {
+  state: DropMenuState;
+  static displayName = Widget.DropMenu;
+  isAutoTriggerWidth: boolean;
+
+  constructor (props: DropMenuProps) {
+    super(props);
+    this.state = { trigerWidth: '100%', };
+    this.isAutoTriggerWidth = false;
+  }
 
   render () {
-    const { children, } = this.props;
-    return <MenuContainer>{children}</MenuContainer>;
+    const { menus, children, } = this.props;
+    const menuConfig = { [Widget.Menu]: { width: this.state.trigerWidth, }, };
+    return <Trigger
+      align="bottomLeft"
+      action="hover"
+      popup={
+        <MenuContainer><Theme config={menuConfig}>{menus}</Theme></MenuContainer>
+      }>
+      {children}
+    </Trigger>;
+  }
+
+  componentDidMount () {
+    this.autoMenuWidth();
+  }
+
+  componentDidUpdate () {
+    this.autoMenuWidth();
+  }
+
+  autoMenuWidth () {
+    if (!this.isAutoTriggerWidth) {
+      this.isAutoTriggerWidth = true;
+      const domNode = ReactDom.findDOMNode(this);
+      if (domNode && domNode.offsetWidth) {
+        this.setState({ trigerWidth: `${String(domNode.offsetWidth)}px`, }, () => {
+          this.isAutoTriggerWidth = false;
+        });
+      }
+    }
   }
 }
 
-export default Menu;
+export default DropMenu;
