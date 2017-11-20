6b347fc79edb5eb3daee4edb710c85bfe94fca0d
zenjava <138962816@qq.com>
2017-11-20 10:39:47 +0800

    修改：   * 增加边框.

--- src/widgets/trigger/PopupInner.js
+++ src/widgets/trigger/PopupInner.js
@@ -8,7 +8,10 @@ import VisibleBox from '../common/VisibleBox';
 import { ZIndex, } from '../common/MaskBox';
 
 const PopupInnerBox = VisibleBox.extend`
-  position: absolute;
+  background-color: #fff;
+  box-shadow: 0 1px 6px rgba(0,0,0,.2);
+  border-radius: 4px;
+  box-sizing: border-box;
   left: -9999px;
   top: -9999px;
   z-index: ${ZIndex + 1};
