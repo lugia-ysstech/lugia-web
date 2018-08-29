3b406a7c254f61b79f3f20093c1a8b6ec30e834d
zenjava <138962816@qq.com>
2018-08-29 14:02:20 +0800

    重构代码：   * 优化代码结构。

--- src/widgets/css/divider.js
+++ src/widgets/css/divider.js
@@ -10,18 +10,10 @@ const { borderDisableColor } = colorsFunc();
 export type DividerType = 'horizontal' | 'vertical';
 export type DividerPosition = 'left' | 'right';
 
-export const getBeforePositionCSS = (props: DividerProps) => {
-  const { position, content } = props;
-  console.log(content);
-  if (content !== undefined) {
-    return `width:${position === 'left' ? '5%' : position === 'right' ? '95%' : '50%'}`;
-  }
-  return 'width :100%';
-};
-export const getAfterPositionCSS = (props: DividerProps) => {
+export const getPositionCSS = ([left, right]: DividerPosition[]) => (props: DividerProps) => {
   const { position, content } = props;
   if (content !== undefined) {
-    return `width:${position === 'left' ? '95%' : position === 'right' ? '5%' : '50%'}`;
+    return `width:${position === left ? '95%' : position === right ? '5%' : '50%'}`;
   }
   return 'width :100%';
 };
--- src/widgets/divider/index.js
+++ src/widgets/divider/index.js
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 import Widget from '../consts/index';
 import { px2emcss } from '../css/units';
 import type { DividerType, DividerPosition } from '../css/divider';
-import { getBeforePositionCSS, getAfterPositionCSS, getColor, getDashed } from '../css/divider';
+import { getPositionCSS, getColor, getDashed } from '../css/divider';
 const em = px2emcss(1.2);
 
 const Divider = styled.div`
@@ -28,7 +28,7 @@ const Divider = styled.div`
     display: table-cell;
     position: relative;
     top: 50%;
-    ${getBeforePositionCSS};
+    ${getPositionCSS(['right', 'left'])};
     border-top: ${em(1)} ${getDashed} ${getColor};
     transform: translateY(50%);
   }
@@ -37,7 +37,7 @@ const Divider = styled.div`
     display: table-cell;
     position: relative;
     top: 50%;
-    ${getAfterPositionCSS};
+    ${getPositionCSS(['left', 'right'])};
     border-top: ${em(1)} ${getDashed} ${getColor};
     transform: translateY(50%);
   }
@@ -77,9 +77,6 @@ class LineBox extends Component<DividerProps, DividerState> {
   };
   static displayName = Widget.Divider;
 
-  constructor(props: dividerProps) {
-    super(props);
-  }
   getChildText() {
     const { content } = this.props;
     if (content !== undefined) {
