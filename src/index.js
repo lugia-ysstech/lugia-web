335b9b30e4fb315d7d21da802a5fd83c129841ed
liangguodong00100 <liang_guodong@126.com>
2018-09-03 20:04:08 +0800

    增加    *Avatar 头像,Badge 徽章标记组件 修改    * theme flow类型检查 增加    * 提取getMargin -> common\ThemeUtils

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactServer from 'react-dom/server';
 
-import App from './widgets/slider/demo'; 
+import App from './widgets/slider/demo';
 //import App from './widgets/switch/demo';
 //import App from './widgets/loading/demo';
 //import App from './widgets/icon/demo';
