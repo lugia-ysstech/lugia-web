76cee182389ac879ef488b9099667f76901b3287
zenjava <138962816@qq.com>
2018-09-20 11:27:39 +0800

    修改：   * 调整与master一致。

--- src/index.js
+++ src/index.js
@@ -5,17 +5,14 @@ import 'core-js/es6/string';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactServer from 'react-dom/server'; 
+import ReactServer from 'react-dom/server';
 
-// import App from './widgets/select/demo';
+import App from './widgets/slider/demo';
 //import App from './widgets/switch/demo';
 //import App from './widgets/loading/demo';
-// import {CheckBoxDemo} from './widgets/checkbox/demo';
-// import CheckBoxDemo from './widgets/checkbox/checkAll';
-// import App from './widgets/checkbox/demo2';
-import App from './widgets/tree-select/demo';
-// import App from './widgets/tree/demo';
-// import App from './widgets/breadcrumb/demo';
+//import App from './widgets/icon/demo';
+//import App from './widgets/tree-select/demo';
+// import App from './widgets/trigger/demo';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
