347d0013ee404b81297ca68bae52d65dfcf58308
zenjava <138962816@qq.com>
2018-10-16 23:27:34 +0800

    修改:   * tag范例

--- src/widgets/tag/demo.js
+++ src/widgets/tag/demo.js
@@ -19,177 +19,13 @@ for (let i = 10; i < 20; i++) {
   data.push(i);
 }
 
-export default class Demo extends React.Component<any, any> {
+export class LimitCase extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = { data };
   }
+
   render() {
-    const config = {
-      [Widget.Tag]: {
-        color: '#ff5588',
-      },
-    };
-
-    const config2 = {
-      [Widget.Tag]: {
-        color: '#fff',
-      },
-    };
-
-    // return (
-    //   <div>
-    //     <div>
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="customs" closeable={false} onClose={this.onClose}>
-    //             customs
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="customs" shape={'round'} closeable={false} onClose={this.onClose}>
-    //             customs
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="customs" closeable={false} onClose={this.onClose}>
-    //           标签
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="customs" onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="customs" shape={'round'} onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-    //     </div>
-
-    //     <div>
-    //       <Box>
-    //         <Theme config={config2}>
-    //           <Tag type="primary" closeable={false} onClose={this.onClose}>
-    //             标签
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Theme config={config2}>
-    //           <Tag type="primary" shape={'round'} closeable={false} onClose={this.onClose}>
-    //             primary
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="primary" closeable={false} onClose={this.onClose}>
-    //           标签
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="primary" onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="primary" shape={'round'} onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-    //     </div>
-
-    //     <div>
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="basic" closeable={false} onClose={this.onClose}>
-    //             basic
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="basic" shape={'round'} closeable={false} onClose={this.onClose}>
-    //             basic
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="basic" closeable={false} onClose={this.onClose}>
-    //           标签
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="basic" onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="basic" shape={'round'} onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-    //     </div>
-
-    //     <div>
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="presets" closeable={false} onClose={this.onClose}>
-    //             presets
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Theme config={config}>
-    //           <Tag type="presets" shape={'round'} closeable={false} onClose={this.onClose}>
-    //             presets
-    //           </Tag>
-    //         </Theme>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="presets" closeable={false} onClose={this.onClose}>
-    //           标签
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag type="presets" onClose={this.onClose}>
-    //           closeable
-    //         </Tag>
-    //       </Box>
-
-    //       <Box>
-    //         <Tag
-    //           type="presets"
-    //           disabled
-    //           shape={'round'}
-    //           onClick={this.onClick}
-    //           onClose={this.onClose}
-    //         >
-    //           closeable
-    //         </Tag>
-    //       </Box>
-    //     </div>
-    //   </div>
-    // );
     return this.getTag();
   }
 
@@ -215,3 +51,188 @@ export default class Demo extends React.Component<any, any> {
     console.log('cc');
   };
 }
+
+export class NormalCase extends React.Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = { data };
+  }
+
+  render() {
+    const config = {
+      [Widget.Tag]: {
+        color: '#ff5588',
+      },
+    };
+
+    const config2 = {
+      [Widget.Tag]: {
+        color: '#fff',
+      },
+    };
+
+    return (
+      <div>
+        <div>
+          <Box>
+            <Theme config={config}>
+              <Tag type="customs" closeable={false} onClose={this.onClose}>
+                customs
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Theme config={config}>
+              <Tag type="customs" shape={'round'} closeable={false} onClose={this.onClose}>
+                customs
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Tag type="customs" closeable={false} onClose={this.onClose}>
+              标签
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="customs" onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="customs" shape={'round'} onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+        </div>
+
+        <div>
+          <Box>
+            <Theme config={config2}>
+              <Tag type="primary" closeable={false} onClose={this.onClose}>
+                标签
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Theme config={config2}>
+              <Tag type="primary" shape={'round'} closeable={false} onClose={this.onClose}>
+                primary
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Tag type="primary" closeable={false} onClose={this.onClose}>
+              标签
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="primary" onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="primary" shape={'round'} onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+        </div>
+
+        <div>
+          <Box>
+            <Theme config={config}>
+              <Tag type="basic" closeable={false} onClose={this.onClose}>
+                basic
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Theme config={config}>
+              <Tag type="basic" shape={'round'} closeable={false} onClose={this.onClose}>
+                basic
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Tag type="basic" closeable={false} onClose={this.onClose}>
+              标签
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="basic" onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="basic" shape={'round'} onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+        </div>
+
+        <div>
+          <Box>
+            <Theme config={config}>
+              <Tag type="presets" closeable={false} onClose={this.onClose}>
+                presets
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Theme config={config}>
+              <Tag type="presets" shape={'round'} closeable={false} onClose={this.onClose}>
+                presets
+              </Tag>
+            </Theme>
+          </Box>
+
+          <Box>
+            <Tag type="presets" closeable={false} onClose={this.onClose}>
+              标签
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag type="presets" onClose={this.onClose}>
+              closeable
+            </Tag>
+          </Box>
+
+          <Box>
+            <Tag
+              type="presets"
+              disabled
+              shape={'round'}
+              onClick={this.onClick}
+              onClose={this.onClose}
+            >
+              closeable
+            </Tag>
+          </Box>
+        </div>
+      </div>
+    );
+  }
+
+  onClose(item, e) {}
+
+  onClick = () => {
+    console.log('cc');
+  };
+}
+
+export default () => {
+  return [<LimitCase />, <NormalCase />];
+};
