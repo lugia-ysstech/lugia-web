d9009056dc813a74e88e29a89901a6f7dcb9c62d
cuicuiwang <1623330976@qq.com>
2018-11-09 14:49:22 +0800

    loading 添加index文件

---
+++ src/widgets/loading/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * create by wangcuixia
+ *
+ * create date: 2018/11/09
+ *
+ * @flow
+ */
+import Widget from '../consts/index';
+import ThemeProvider from '../theme-provider';
+import Loading from './loading';
+
+export default ThemeProvider(Loading, Widget.Loading);
