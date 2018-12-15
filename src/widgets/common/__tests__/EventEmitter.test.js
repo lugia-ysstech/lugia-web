06dfb057385d1fae1a44ce34e9c9469b46665f79
zenjava <138962816@qq.com>
2018-12-15 17:06:06 +0800

    修改：   * 增加once removeListener removeAllListener

--- src/widgets/common/__tests__/EventEmitter.test.js
+++ src/widgets/common/__tests__/EventEmitter.test.js
@@ -8,7 +8,7 @@
 import Listener from '../EventEmitter';
 
 describe('Listener', () => {
-  it('removeListener click', async () => {
+  it('removeListener on click', async () => {
     let call = false;
     const cb = () => {
       call = true;
@@ -19,6 +19,22 @@ describe('Listener', () => {
     const listener = new Listener();
     const eventName = 'click';
     const res = listener.on(eventName, cb);
+    res.removeListener();
+    listener.emit(eventName, mouseEventObj);
+    expect(call).toBeFalsy();
+  });
+
+  it('removeListener  once click', async () => {
+    let call = false;
+    const cb = () => {
+      call = true;
+    };
+    const mouseEventObj = {
+      buttons: 1,
+    };
+    const listener = new Listener();
+    const eventName = 'click';
+    const res = listener.once(eventName, cb);
     res.removeListener();
     listener.emit(eventName, mouseEventObj);
     expect(call).toBeFalsy();
@@ -60,10 +76,13 @@ describe('Listener', () => {
 
   it('on click', async () => {
     let cb = () => {};
-
+    const params = [];
     const callPromise = new Promise(res => {
       cb = data => {
-        res(data);
+        params.push(data);
+        if (params.length === 2) {
+          res(params);
+        }
       };
     });
     const mouseEventObj = {
@@ -73,6 +92,44 @@ describe('Listener', () => {
     const eventName = 'click';
     listener.on(eventName, cb);
     listener.emit(eventName, mouseEventObj);
+    listener.emit(eventName, mouseEventObj);
+    expect(await callPromise).toEqual([mouseEventObj, mouseEventObj]);
+  });
+
+  it('once click', async () => {
+    let cb = () => {};
+    let cnt = 0;
+    const callPromise = new Promise(res => {
+      cb = data => {
+        cnt++;
+        res(data);
+      };
+    });
+    const mouseEventObj = {
+      buttons: 1,
+    };
+    const listener = new Listener();
+    const eventName = 'click';
+    listener.once(eventName, cb);
+    listener.emit(eventName, mouseEventObj);
+    listener.emit(eventName, mouseEventObj);
     expect(await callPromise).toBe(mouseEventObj);
+    expect(cnt).toBe(1);
+  });
+
+  it('removeAllListeners', async () => {
+    let cnt = 0;
+    const cb = data => {
+      cnt++;
+    };
+    const mouseEventObj = {
+      buttons: 1,
+    };
+    const listener = new Listener();
+    const eventName = 'click';
+    listener.on(eventName, cb);
+    listener.removeAllListeners();
+    listener.emit(eventName, mouseEventObj);
+    expect(cnt).toBe(0);
   });
 });
