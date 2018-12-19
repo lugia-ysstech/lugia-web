794aa26f3ef0c584e2fab3355694ae6812c034b8
grguang <945455382@qq.com>
2018-12-19 14:04:23 +0800

    *修改：     * transfer删除 group.js 注释代码；

--- src/widgets/transfer/group.js
+++ src/widgets/transfer/group.js
@@ -18,7 +18,6 @@ import {
   getPanelSourceDataAndTargetData,
   getTreeData,
   getTruthValue,
-  splitSelectKeys,
 } from './utils';
 import { getMapData } from './menu-utils';
 import TransferModel from './model';
@@ -110,8 +109,8 @@ export default ThemeProvider(
         targetKeys = [],
       } = nextProps;
       if (nextProps.data.length !== this.props.data.length || nextProps.data !== this.props.data) {
-        let mapData;
         const theTargetKeys = this.getTargetKeys(nextProps);
+        let mapData;
         if (type === 'panel') {
           mapData = getMapData(data, valueField);
           const { targetCheckKeys, sourceCheckKeys } = getPanelSourceDataAndTargetData(
@@ -191,7 +190,6 @@ export default ThemeProvider(
             displayField={displayField}
             valueField={valueField}
             type={type}
-            // 左侧 黑单
           />
           <OperationBtn>
             <Button
@@ -212,27 +210,17 @@ export default ThemeProvider(
           </OperationBtn>
           <TransFer
             key="2"
-            // displayField={displayField}
-            // valueField={valueField}
             model={this.targetModel}
             direction="Target"
-            // type={type}
             onSelect={this.handleTargetSelect}
-            // data={theTargetData}
-            // showSearch={showSearch}
             onCheckAll={this.checkAllForRight}
-            // canCheckKeys={targetCheckKeys}
-            // onSearch={this.searchCallbackForRight}
             needCancelBox
-            // cancelItem={cancelItem}
             onCancelItemClick={this.handleCancelItemClick}
             title="列表B"
             {...this.props}
             displayField={displayField}
             valueField={valueField}
             type={type}
-            // onSelect={this.handleSelect}
-            //右侧 白单
           />
         </TransFerWrap>
       );
@@ -240,7 +228,6 @@ export default ThemeProvider(
     handleSourceSelect = (item: string[]) => {
       const { onSelectChange } = this.props;
       const targetSelectedKeys = this.targetModel.getSelectedkeys();
-      console.info('source selected', item);
       onSelectChange && onSelectChange(item, targetSelectedKeys);
 
       const hasSourceSelectedKeys = this.isInProps('sourceSelectedKeys');
@@ -253,7 +240,6 @@ export default ThemeProvider(
     handleTargetSelect = (item: string[]) => {
       const { onSelectChange } = this.props;
       const sourceSelectedKeys = this.sourceModel.getSelectedkeys();
-      console.info('target selected', item);
       onSelectChange && onSelectChange(sourceSelectedKeys, item);
 
       const hasTargetSelectedKeys = this.isInProps('targetSelectedKeys');
@@ -324,7 +310,6 @@ export default ThemeProvider(
 
     getEnableKeys = (type: 'panel' | 'tree', nextTargetKeys: string[]) => {
       const { data, valueField = 'value' } = this.props;
-      // const theTargetKeys = this.sourceModel.getList();
       if (type === 'panel') {
         const { targetCheckKeys, sourceCheckKeys } = getPanelSourceDataAndTargetData(
           data,
@@ -354,33 +339,19 @@ export default ThemeProvider(
     }
 
     checkAllForLeft = (checked: boolean) => {
-      // const { mapData, sourceSelectedKeys, targetSelectedKeys, sourceCheckKeys } = this.state;
       const { onSelectChange } = this.props;
-      const sourceSelectedKeys = this.sourceModel.getSelectedkeys();
-      const disabledCheckedKeys = splitSelectKeys(this.sourceModel.getMapData(), sourceSelectedKeys)
-        .disabledKeys;
-      //
-      // const checkKeys = checked
-      //   ? [...sourceCheckKeys, ...disabledCheckedKeys]
-      //   : disabledCheckedKeys || [];
       const checkKeys = this.sourceModel.getCheckAllKeys(checked);
       const targetSelectedKeys = this.targetModel.getSelectedkeys();
-      console.info('checked', checked);
-      console.info('checkKeys', checkKeys);
-      // console.info('targetSelectedKeys',targetSelectedKeys);
+
       onSelectChange && onSelectChange(checkKeys, targetSelectedKeys);
 
       const hasSourceSelectedKeys = this.isInProps('sourceSelectedKeys');
       if (hasSourceSelectedKeys) {
         return;
       }
-      // this.setState({
-      //   sourceSelectedKeys: checkKeys,
-      // });
       this.sourceModel.changeSelectedKeys(checkKeys);
     };
     checkAllForRight = (checked: boolean) => {
-      // const { mapData, sourceSelectedKeys, targetSelectedKeys, targetCheckKeys } = this.state;
       const { onSelectChange } = this.props;
       const checkKeys = this.targetModel.getCheckAllKeys(checked);
       const sourceSelectedKeys = this.sourceModel.getSelectedkeys();
@@ -390,66 +361,10 @@ export default ThemeProvider(
       if (hasTargetSelectedKeys) {
         return;
       }
-      // this.setState({
-      //   targetSelectedKeys: checkKeys,
-      // });
       this.targetModel.changeSelectedKeys(checkKeys);
     };
-    getCheckKeys = (
-      mapData: Object,
-      selectKey: string[],
-      allCanCheckKeys: string[],
-      checked: boolean
-    ) => {
-      const { disabledKeys: disabledCheckedKeys } = splitSelectKeys(mapData, selectKey);
-      const checkKeys = checked
-        ? [...allCanCheckKeys, ...disabledCheckedKeys]
-        : disabledCheckedKeys || [];
-
-      return checkKeys;
-    };
-
-    // searchCallbackForLeft = (inputValue: string) => {
-    //   const { sourceData = [] } = this.state;
-    //   this.sourceInputValue = inputValue;
-    //   this.setSearchData(sourceData, inputValue, 'sourceSearchData');
-    // };
-    // searchCallbackForRight = (inputValue: string) => {
-    //   const { targetData = [] } = this.state;
-    //   this.targetInputValue = inputValue;
-    //   this.setSearchData(targetData, inputValue, 'targetSearchData');
-    // };
-    // setSearchData = (data: Object[], inputValue: string, target: string) => {
-    //   const SearchData = this.searchFilter(data, inputValue);
-    //   this.setState({ [target]: inputValue ? SearchData : data });
-    // };
-    // searchFilter = (data: Object[], searchValue: string) => {
-    //   const { valueField = ValueField } = this.props;
-    //   const {
-    //     filterOption = (value, option) => {
-    //       return option[valueField].indexOf(value) > -1;
-    //     },
-    //   } = this.props;
-    //   if (data && data.length > 0 && filterOption && typeof filterOption === 'function') {
-    //     const searchData = [];
-    //     if (this.maping) {
-    //       return;
-    //     }
-    //     if (!this.maping) {
-    //       this.maping = true;
-    //       data.forEach(item => {
-    //         if (filterOption(searchValue, item)) {
-    //           searchData.push(item);
-    //         }
-    //       });
-    //       this.maping = false;
-    //       return searchData;
-    //     }
-    //   }
-    // };
 
     handleCancelItemClick = (value: string) => {
-      // const { displayValue, targetKeys } = this.state;
       const displayValue = this.targetModel.getDisplayValue();
       const targetKeys = this.targetModel.getList();
       const newDisplayValue = [...displayValue];
@@ -478,10 +393,6 @@ export default ThemeProvider(
       this.targetModel.setDisplayValue(newDisplayValue);
       this.targetModel.changeList(newTargetKeys);
       this.sourceModel.changeList(newTargetKeys);
-      // this.setState({
-      //   displayValue: newDisplayValue,
-      //   targetKeys: newTargetKeys,
-      // });
     };
     isInProps(value: string) {
       return value in this.props;
