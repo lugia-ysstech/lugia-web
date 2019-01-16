e283edc6daa9f132d04293be1d745b9c14b1248d
liangguodong00100 <liang_guodong@126.com>
2019-01-16 15:52:16 +0800

    增加     *screen-shot 组件缩略图.

---
+++ src/widgets/screen-shot/index.js
@@ -0,0 +1,89 @@
+/**
+ *
+ * create by liangguodong
+ *
+ * @flow
+ */
+export { default as Upload } from './上传.png';
+
+export { default as Select } from './下拉菜单.png';
+
+export { default as Divider } from './分割线.png';
+
+export { default as Pagination } from './分页.png';
+
+export { default as List } from './列表.png';
+
+export { default as Check } from './单选框.png';
+
+export { default as Card } from './卡片.png';
+
+export { default as Affix } from './固钉.png';
+
+export { default as CheckBox } from './多选框.png';
+
+export { default as Avatar } from './头像.png';
+
+export { default as Modal } from './对话框.png';
+
+export { default as NavMenu } from './导航菜单.png';
+
+export { default as Badge } from './小红点.png';
+
+export { default as Switch } from './开关.png';
+
+export { default as Notification } from './弹窗提醒.png';
+
+export { default as Collapse } from './折叠面板.png';
+
+export { default as Drawer } from './抽屉.png';
+
+export { default as Button } from './按钮.png';
+
+export { default as Mention } from './提及.png';
+
+export { default as NumberInput } from './数字选择器.png';
+
+export { default as ToolTip } from './文字提醒.png';
+
+export { default as Calendar } from './日历.png';
+
+export { default as Timeline } from './时间轴.png';
+
+export { default as DatePicker } from './时间选择器.png';
+
+export { default as Tag } from './标签.png';
+
+export { default as Tabs } from './标签页.png';
+
+export { default as Tree } from './树形控件.png';
+
+export { default as TreeSelect } from './树选择.png';
+
+export { default as Steps } from './步骤条.png';
+
+export { default as Popover } from './气泡卡片.png';
+
+export { default as Slider } from './滑块.png';
+
+export { default as Transfer } from './穿梭框.png';
+
+export { default as AutoComplete } from './自动完成.png';
+
+export { default as Table } from './表格.png';
+
+export { default as Alert } from './警告信息.png';
+
+export { default as Rate } from './评分.png';
+
+export { default as Carousel } from './走马灯.png';
+
+export { default as Input } from './输入框.png';
+
+export { default as BackTop } from './返回顶部.png';
+
+export { default as Progress } from './进度条.png';
+
+export { default as Anchor } from './锚点.png';
+
+export { default as Breadcrumb } from './面包屑.png';
