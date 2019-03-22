d1527fe11b4822dfe4d59826a29a19aa83e6a1ea
zenjava <138962816@qq.com>
2019-03-22 20:37:01 +0800

    修改：   * 缩略图

--- src/widgets/screen-shot/index.js
+++ src/widgets/screen-shot/index.js
@@ -4,9 +4,11 @@
  *
  * @flow
  */
+export { default as Skeleton } from './card.png';
 export { default as Upload } from './upload.png';
 
 export { default as Select } from './select.png';
+export { default as Cascader } from './select.png';
 
 export { default as Divider } from './divider.png';
 
@@ -20,7 +22,8 @@ export { default as Card } from './card.png';
 
 export { default as Affix } from './affix.png';
 
-export { default as CheckBox } from './checkBox.png';
+export { default as Checkbox } from './checkBox.png';
+export { default as CheckboxGroup } from './checkBox.png';
 
 export { default as Avatar } from './avatar.png';
 
@@ -35,6 +38,7 @@ export { default as Switch } from './switch.png';
 export { default as Notification } from './notification.png';
 
 export { default as Collapse } from './collapse.png';
+export { default as Panel } from './card.png';
 
 export { default as Drawer } from './drawer.png';
 
@@ -48,9 +52,11 @@ export { default as ToolTip } from './toolTip.png';
 
 export { default as Calendar } from './calendar.png';
 
-export { default as Timeline } from './timeline.png';
+export { default as TimeLine } from './timeline.png';
 
 export { default as DatePicker } from './datePicker.png';
+export { default as TimePicker } from './datePicker.png';
+export { default as Tooltip } from './toolTip.png';
 
 export { default as Tag } from './tag.png';
 
@@ -63,14 +69,25 @@ export { default as TreeSelect } from './treeSelect.png';
 export { default as Steps } from './steps.png';
 
 export { default as Popover } from './popover.png';
+export { default as Popconfirm } from './popover.png';
 
 export { default as Slider } from './slider.png';
 
 export { default as Transfer } from './transfer.png';
 
 export { default as AutoComplete } from './autoComplete.png';
+export { default as AmountInput } from './numberInput.png';
+export { default as Dropmenu } from './navMenu.png';
+export { default as Menu } from './navMenu.png';
 
 export { default as Table } from './table.png';
+export { default as Grid } from './card.png';
+export { default as Aside } from './card.png';
+export { default as Loading } from './progress.png';
+export { default as Col } from './card.png';
+export { default as Row } from './card.png';
+export { default as Icon } from './affix.png';
+export { default as Layout } from './card.png';
 
 export { default as Alert } from './alert.png';
 
