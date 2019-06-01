29691f445d15f1df584906581fee4f1a456090ea
zenjava <138962816@qq.com>
2019-06-01 10:16:37 +0800

    修改：   * 更新lugia-theme的包依赖；   * 将theme-css-provider替换为theme-css-hoc

--- src/widgets/theme/CSSProvider.js
+++ src/widgets/theme/CSSProvider.js
@@ -5,7 +5,7 @@
  * @flow
  */
 
-export { default } from '@lugia/theme-css-provider';
-export { StaticComponent } from '@lugia/theme-css-provider';
-export { css } from '@lugia/theme-css-provider';
-export { keyframes } from '@lugia/theme-css-provider';
+export { default } from '@lugia/theme-css-hoc';
+export { StaticComponent } from '@lugia/theme-css-hoc';
+export { css } from '@lugia/theme-css-hoc';
+export { keyframes } from '@lugia/theme-css-hoc';
