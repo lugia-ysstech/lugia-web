4afd95e97988409813565e3bcce5815eee3d8f44
zenjava <138962816@qq.com>
2019-06-06 16:59:58 +0800

    修改：   * 将demo的查看集成为路由的方式。

--- src/App.js
+++ src/App.js
@@ -1,258 +1,291 @@
-/* eslint no-console:0 */
+import React, { Component } from 'react';
+import { createRoute, Link } from '@lugia/lugiax-router';
+import router from './router';
+import Menu from './widgets/menu';
+import Input from './widgets/input';
 
-import React from 'react';
-import Trigger from './widgets/trigger';
-import assign from 'object-assign';
-
-function getPopupAlign(state) {
-  return {
-    offset: [state.offsetX, state.offsetY],
-    overflow: {
-      adjustX: 1,
-      adjustY: 1,
-    },
-  };
-}
-
-const builtinPlacements = {
-  left: {
-    points: ['cr', 'cl'],
+const data = [
+  {
+    value: '/affix',
+    sort: 35,
+    text: 'Affix 固钉',
   },
-  right: {
-    points: ['cl', 'cr'],
+  {
+    value: '/navmenu',
+    sort: 36,
+    text: 'NavMenu 导航菜单',
   },
-  top: {
-    points: ['bc', 'tc'],
+  {
+    value: '/pagination',
+    sort: 37,
+    text: 'Pagination 分页',
   },
-  bottom: {
-    points: ['tc', 'bc'],
+  {
+    value: '/steps',
+    sort: 38,
+    text: 'Steps 步骤条',
   },
-  topLeft: {
-    points: ['bl', 'tl'],
+  {
+    value: '/alert',
+    sort: 39,
+    text: 'Alert 警告提示',
   },
-  topRight: {
-    points: ['br', 'tr'],
+  {
+    value: '/drawer',
+    sort: 40,
+    text: 'Drawer 抽屉',
   },
-  bottomRight: {
-    points: ['tr', 'br'],
+  {
+    value: '/message',
+    sort: 41,
+    text: 'Message 全局提示',
   },
-  bottomLeft: {
-    points: ['tl', 'bl'],
+  {
+    value: '/modal',
+    sort: 42,
+    text: 'Modal 对话框',
   },
-};
+  {
+    value: '/notification',
+    sort: 43,
+    text: 'Notification 通知提醒框',
+  },
+  {
+    value: '/popconfirm',
+    sort: 44,
+    text: 'Popconfirm 气泡确认框',
+  },
+  {
+    value: '/progress',
+    sort: 45,
+    text: 'Progress 进度条',
+  },
+  {
+    value: '/skeleton',
+    sort: 46,
+    text: 'Skeleton 加载占位符',
+  },
+  {
+    value: '/amountinput',
+    sort: 47,
+    text: 'AmountInput 金额输入框',
+  },
+  {
+    value: '/autocomplete',
+    sort: 48,
+    text: 'AutoComplete 自动完成',
+  },
+  {
+    value: '/cascader',
+    sort: 49,
+    text: 'Cascader 级联选择',
+  },
+  {
+    value: '/checkbox',
+    sort: 50,
+    text: 'Checkbox 多选框',
+  },
+  {
+    value: '/datepicker',
+    sort: 51,
+    text: 'DatePicker 日期选择器',
+  },
+  {
+    value: '/dropmenu',
+    sort: 52,
+    text: 'Dropmenu 下拉菜单',
+  },
+  {
+    value: '/input',
+    sort: 53,
+    text: 'Input 文本输入框',
+  },
+  {
+    value: '/menu',
+    sort: 54,
+    text: 'Menu 菜单',
+  },
+  {
+    value: '/numberinput',
+    sort: 55,
+    text: 'NumberInput 数字输入框',
+  },
+  {
+    value: '/radio',
+    sort: 56,
+    text: 'Radio 单选框',
+  },
+  {
+    value: '/rate',
+    sort: 57,
+    text: 'Rate 评分',
+  },
+  {
+    value: '/select',
+    sort: 58,
+    text: 'Select 选择器',
+  },
+  {
+    value: '/slider',
+    sort: 59,
+    text: 'Slider 滑动输入条',
+  },
+  {
+    value: '/switch',
+    sort: 60,
+    text: 'Switch 开关',
+  },
+  {
+    value: '/timepicker',
+    sort: 61,
+    text: 'TimePicker 时间选择器',
+  },
+  {
+    value: '/transfer',
+    sort: 62,
+    text: 'Transfer 穿梭框',
+  },
+  {
+    value: '/tree',
+    sort: 63,
+    text: 'Tree 树形控件',
+  },
+  {
+    value: '/treeselect',
+    sort: 64,
+    text: 'TreeSelect 树形选择控件',
+  },
+  {
+    value: '/upload',
+    sort: 65,
+    text: 'Upload 上传',
+  },
+  {
+    value: '/anchor',
+    sort: 66,
+    text: 'Anchor 锚点',
+  },
+  {
+    value: '/backtop',
+    sort: 67,
+    text: 'BackTop 回到顶部',
+  },
+  {
+    value: '/divider',
+    sort: 68,
+    text: 'Divider 分割线',
+  },
+  {
+    value: '/loading',
+    sort: 69,
+    text: 'Loading 加载中',
+  },
+  {
+    value: '/avatar',
+    sort: 70,
+    text: 'Avatar 头像',
+  },
+  {
+    value: '/badge',
+    sort: 71,
+    text: 'Badge 徽标数',
+  },
+  {
+    value: '/card',
+    sort: 72,
+    text: 'Card 卡片',
+  },
+  {
+    value: '/carousel',
+    sort: 73,
+    text: 'Carousel 走马灯',
+  },
+  {
+    value: '/collapse',
+    sort: 74,
+    text: 'Collapse 折叠面板',
+  },
+  {
+    value: '/popover',
+    sort: 75,
+    text: 'Popover 气泡卡片',
+  },
+  {
+    value: '/table',
+    sort: 76,
+    text: 'Table 表格',
+  },
+  {
+    value: '/tabs',
+    sort: 77,
+    text: 'Tabs 标签页',
+  },
+  {
+    value: '/tag',
+    sort: 78,
+    text: 'Tag 标签',
+  },
+  {
+    value: '/timeline',
+    sort: 79,
+    text: 'TimeLine 时间轴',
+  },
+  {
+    value: '/tooltip',
+    sort: 80,
+    text: 'Tooltip 文字提示',
+  },
+  {
+    value: '/breadcrumb',
+    sort: 81,
+    text: 'Breadcrumb 面包屑',
+  },
+  {
+    value: '/button',
+    text: 'Button 按钮',
+  },
+  {
+    value: '/icon',
+    text: 'Icon 图标',
+  },
+  {
+    value: '/grid',
+    text: 'Grid 栅格',
+  },
+  {
+    value: '/layout',
+    text: 'Layout 布局',
+  },
+];
 
-function preventDefault(e) {
-  e.preventDefault();
-}
+class Header extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = { data };
+  }
 
-function getPopupContainer(trigger) {
-  return trigger.parentNode;
-}
-
-class Test extends React.Component {
-  state = {
-    mask: false,
-    maskClosable: false,
-    placement: 'right',
-    trigger: {
-      hover: 1,
-    },
-    offsetX: undefined,
-    offsetY: undefined,
+  wrapItem = (item: Object, param: Object) => {
+    const { key } = param;
+    return <Link to={`${key}`}>{item} </Link>;
   };
 
-  onPlacementChange = e => {
+  onChange = item => {
+    const { newValue = '' } = item;
     this.setState({
-      placement: e.target.value,
-    });
-  };
-
-  onTransitionChange = e => {
-    this.setState({
-      transitionName: e.target.checked ? e.target.value : '',
-    });
-  };
-
-  onTriggerChange = e => {
-    const trigger = assign({}, this.state.trigger);
-    if (e.target.checked) {
-      trigger[e.target.value] = 1;
-    } else {
-      delete trigger[e.target.value];
-    }
-    this.setState({
-      trigger,
-    });
-  };
-
-  onOffsetXChange = e => {
-    const targetValue = e.target.value;
-    this.setState({
-      offsetX: targetValue || undefined,
-    });
-  };
-
-  onOffsetYChange = e => {
-    const targetValue = e.target.value;
-    this.setState({
-      offsetY: targetValue || undefined,
-    });
-  };
-
-  onVisibleChange = visible => {
-    console.log('tooltip', visible);
-  };
-
-  onMask = e => {
-    this.setState({
-      mask: e.target.checked,
-    });
-  };
-
-  onMaskClosable = e => {
-    this.setState({
-      maskClosable: e.target.checked,
-    });
-  };
-
-  destroy = () => {
-    this.setState({
-      destroyed: true,
-    });
-  };
-
-  destroyPopupOnHide = e => {
-    this.setState({
-      destroyPopupOnHide: e.target.checked,
+      data: data.filter(({ value }) => {
+        return value && value.toLowerCase().indexOf(newValue.toLowerCase()) !== -1;
+      }),
     });
   };
 
   render() {
-    const state = this.state;
-    const trigger = state.trigger;
-    if (state.destroyed) {
-      return null;
-    }
-    return (
-      <div>
-        <div style={{ margin: '10px 20px' }}>
-          <label>
-            placement:
-            <select value={state.placement} onChange={this.onPlacementChange}>
-              <option>right</option>
-              <option>left</option>
-              <option>top</option>
-              <option>bottom</option>
-              <option>topLeft</option>
-              <option>topRight</option>
-              <option>bottomRight</option>
-              <option>bottomLeft</option>
-            </select>
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <label>
-            <input
-              value="rc-trigger-popup-zoom"
-              type="checkbox"
-              onChange={this.onTransitionChange}
-              checked={state.transitionName === 'rc-trigger-popup-zoom'}
-            />
-            transitionName
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp; trigger:
-          <label>
-            <input
-              value="hover"
-              checked={!!trigger.hover}
-              type="checkbox"
-              onChange={this.onTriggerChange}
-            />
-            hover
-          </label>
-          <label>
-            <input
-              value="focus"
-              checked={!!trigger.focus}
-              type="checkbox"
-              onChange={this.onTriggerChange}
-            />
-            focus
-          </label>
-          <label>
-            <input
-              value="click"
-              checked={!!trigger.click}
-              type="checkbox"
-              onChange={this.onTriggerChange}
-            />
-            click
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <label>
-            <input
-              checked={!!this.state.destroyPopupOnHide}
-              type="checkbox"
-              onChange={this.destroyPopupOnHide}
-            />
-            destroyPopupOnHide
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <label>
-            <input checked={!!this.state.mask} type="checkbox" onChange={this.onMask} />
-            mask
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <label>
-            <input
-              checked={!!this.state.maskClosable}
-              type="checkbox"
-              onChange={this.onMaskClosable}
-            />
-            maskClosable
-          </label>
-          <br />
-          <label>
-            offsetX:
-            <input type="text" onChange={this.onOffsetXChange} style={{ width: 50 }} />
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <label>
-            offsetY:
-            <input type="text" onChange={this.onOffsetYChange} style={{ width: 50 }} />
-          </label>
-          &nbsp;&nbsp;&nbsp;&nbsp;
-          <button onClick={this.destroy}>destroy</button>
-        </div>
-        <div style={{ margin: 100, position: 'relative' }}>
-          <Trigger
-            offsetX={this.state.offsetX}
-            offsetY={this.state.offsetY}
-            getPopupContainer={undefined && getPopupContainer}
-            align={state.placement.toString()}
-            destroyPopupOnHide={this.state.destroyPopupOnHide}
-            // zIndex={40}
-            mask={this.state.mask}
-            maskClosable={this.state.maskClosable}
-            // maskAnimation="fade"
-            // mouseEnterDelay={0.1}
-            // mouseLeaveDelay={0.1}
-            action={Object.keys(state.trigger)}
-            builtinPlacements={builtinPlacements}
-            popup={
-              <div style={{ border: '1px solid red', padding: 10, background: 'white' }}>
-                i am a popup
-              </div>
-            }
-            popupTransitionName={state.transitionName}
-          >
-            <a href="#" style={{ margin: 20 }} onClick={preventDefault}>
-              trigger
-            </a>
-          </Trigger>
-        </div>
-      </div>
-    );
+    return [
+      <Input onChange={this.onChange} />,
+      <Menu data={this.state.data} wrapItem={this.wrapItem} />,
+    ];
   }
 }
 
-export default Test;
+export default () => {
+  return [<Header />, createRoute(router)];
+};
