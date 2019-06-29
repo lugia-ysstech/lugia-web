0c1f9d5b924b8aeab0f5bbc51e912c2d78dc61e2
liangguodong00100 <liang_guodong@126.com>
2019-06-29 18:28:00 +0800

    修改     * Popover修改第一步

--- src/widgets/popover/popover.js
+++ src/widgets/popover/popover.js
@@ -8,65 +8,117 @@ import * as React from 'react';
 import Tooltip from '../tooltip/index';
 import Icon from '../icon/index';
 import Widget from '../consts/index';
-import styled from 'styled-components';
-import { getDescripitionColor, getTitleColor } from '../css/card';
 import type { PopoverProps, PopoverState } from '../css/popover';
-import { getIconColor } from '../css/popover';
-import { px2emcss } from '../css/units';
-import ThemeProvider from '../theme-provider';
 import { ObjectUtils } from '@lugia/type-utils';
 import { getStateFromProps, processOnVisibleChange } from '../tooltip';
 
-const em = px2emcss(1.2);
+import ThemeHoc from '@lugia/theme-hoc';
+import CSSComponent, { css } from '../theme/CSSProvider';
+import { units } from '@lugia/css';
+import colorsFunc from '../css/stateColor';
 
-const ClearContainer = styled.div`
-  position: absolute;
-  right: ${em(8)};
-  top: ${em(6)};
-  width: ${em(10)};
-  height: ${em(10)};
-`;
-const Clear: Object = styled(Icon)`
-  font-size: 1rem;
-  ${getIconColor};
-`;
-const Basetext = styled.div`
-  text-align: inherit;
-  white-space: nowrap;
-  overflow: hidden;
-`;
-const Title = styled(Basetext)`
-  display: inline-block;
-  font-size: 1.6rem;
-  font-weight: 500;
-  ${getTitleColor};
-`;
-const Descripition = styled(Basetext)`
-  font-size: 1.4rem;
-  ${getDescripitionColor};
-`;
-const Content = styled.div`
-  font-size: 1.2rem;
-  display: inline-block;
-  padding: ${em(6)} ${em(8)};
-`;
+const { px2remcss } = units;
+const { mediumGreyColor, darkGreyColor, blackColor } = colorsFunc();
 
-const TooltipWrapper: Object = styled(Tooltip)`
-  font-size: 1.2rem;
-  display: inline-block;
-  position: relative;
-`;
+const ClearContainer: Object = CSSComponent({
+  tag: 'div',
+  className: 'PopoverIconContainer',
+  normal: {
+    selectNames: [[]],
+    defaultTheme: {},
+  },
+  css: css`
+    position: absolute;
+    right: ${px2remcss(8)};
+    top: ${px2remcss(6)};
+    width: ${px2remcss(10)};
+    height: ${px2remcss(10)};
+  `,
+});
+const Clear: Object = CSSComponent({
+  extend: Icon,
+  className: 'PopoverClearIcon',
+  normal: {
+    selectNames: [['color'], ['fontSize'], ['font']],
+    defaultTheme: { color: mediumGreyColor, fontSize: 10 },
+  },
+});
+const BaseText: Object = CSSComponent({
+  tag: 'div',
+  className: 'PopoverBaseText',
+  normal: {},
+  css: css`
+    text-align: inherit;
+    white-space: nowrap;
+    overflow: hidden;
+  `,
+});
+const Title: Object = CSSComponent({
+  extend: BaseText,
+  className: 'PopoverTitle',
+  normal: {
+    selectNames: [['font'], ['fontSize'], ['color'], ['margin']],
+    defaultTheme: {
+      font: {
+        size: 16,
+        color: blackColor,
+        weight: 500,
+      },
+    },
+  },
+  css: css`
+    display: inline-block;
+  `,
+});
+const Description: Object = CSSComponent({
+  extend: BaseText,
+  className: 'PopoverDescription',
+  normal: {
+    selectNames: [['font'], ['fontSize'], ['color'], ['margin']],
+    defaultTheme: {
+      fontSize: 14,
+      color: darkGreyColor,
+    },
+  },
+});
+const Content: Object = CSSComponent({
+  tag: 'div',
+  className: 'PopoverContent',
+  normal: {
+    selectNames: [['font'], ['fontSize'], ['color'], ['padding']],
+    defaultTheme: {
+      padding: {
+        top: 6,
+        bottom: 6,
+        left: 8,
+        right: 8,
+      },
+      fontSize: 12,
+    },
+  },
+  css: css`
+    display: inline-block;
+  `,
+});
 
-const DefaultChild = styled.div`
-  width: ${em(200)};
-  height: ${em(20)};
-`;
+const TooltipWrapper: Object = CSSComponent({
+  extend: Tooltip,
+  className: 'PopoverWrapper',
+  normal: {
+    selectNames: [['font'], ['fontSize'], ['color']],
+    defaultTheme: { fontSize: 12 },
+  },
+  css: css`
+    display: inline-block;
+    position: relative;
+  `,
+});
+
 class Popover extends React.Component<PopoverProps, PopoverState> {
   static displayName = Widget.Popover;
   static defaultProps = {
     defaultVisible: false,
     action: ['click'],
-    defaultChildren: <DefaultChild>"点击显示气泡卡片"</DefaultChild>,
   };
   target: Object;
 
@@ -79,19 +131,21 @@ class Popover extends React.Component<PopoverProps, PopoverState> {
   }
 
   getTitle(): React.Node | null {
-    const { title } = this.props;
-    return title ? <Title>{title} </Title> : null;
+    const { title, themeProps } = this.props;
+    return title ? <Title themeProps={themeProps}>{title} </Title> : null;
   }
 
-  getDescripition(): React.Node | null {
-    const { description } = this.props;
-    return description ? <Descripition>{description} </Descripition> : null;
+  getDescription(): React.Node | null {
+    const { description, themeProps } = this.props;
+    return description ? <Description themeProps={themeProps}>{description} </Description> : null;
   }
 
   getCloseContainer(): React.Node | null {
-    const { clear } = this.props;
+    const { clear, themeProps } = this.props;
     return clear ? (
-      <ClearContainer onClick={this.onClearClick}>{this.getIcon(clear)}</ClearContainer>
+      <ClearContainer themeProps={themeProps} onClick={this.onClearClick}>
+        {this.getIcon(clear)}
+      </ClearContainer>
     ) : null;
   }
 
@@ -106,14 +160,14 @@ class Popover extends React.Component<PopoverProps, PopoverState> {
   };
 
   getContent() {
-    const { content, title, description } = this.props;
+    const { content, title, description, themeProps } = this.props;
     if (content || title || description) {
       return (
-        <Content>
+        <Content themeProps={themeProps}>
           {content}
           {this.getCloseContainer()}
           {this.getTitle()}
-          {this.getDescripition()}
+          {this.getDescription()}
         </Content>
       );
     }
@@ -121,22 +175,21 @@ class Popover extends React.Component<PopoverProps, PopoverState> {
   }
 
   render() {
-    const { children, action, placement, getTheme, defaultChildren } = this.props;
+    const { children = <div />, action, placement, themeProps } = this.props;
     const { visible } = this.state;
     const getTarget: Function = cmp => (this.target = cmp);
-    const theChildren = children ? children : defaultChildren;
     return (
       <TooltipWrapper
         visible={visible}
         action={action}
         onVisibleChange={this.onVisibleChange}
-        theme={getTheme()}
+        themeProps={themeProps}
         popArrowType={'round'}
         placement={placement}
         title={this.getContent()}
         ref={getTarget}
       >
-        {theChildren}
+        {children}
       </TooltipWrapper>
     );
   }
@@ -146,4 +199,7 @@ class Popover extends React.Component<PopoverProps, PopoverState> {
   };
 }
 
-export default ThemeProvider(Popover, Widget.Popover);
+export default ThemeHoc(Popover, Widget.Popover, {
+  hover: true,
+  active: true,
+});
