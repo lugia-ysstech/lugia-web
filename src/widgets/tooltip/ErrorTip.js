ef0f925f20a03689a1316353af448683236ef2ee
liangguodong00100 <liang_guodong@126.com>
2019-06-29 17:16:21 +0800

    修改     * toolTip组件,优化结构 配置文件增加theme信息

--- src/widgets/tooltip/ErrorTip.js
+++ src/widgets/tooltip/ErrorTip.js
@@ -8,22 +8,42 @@ import * as React from 'react';
 import Tooltip from './';
 import Widget from '../consts/index';
 import Theme from '../theme';
+import colorsFunc from '../css/stateColor';
 
+const { lightGreyColor, dangerColor } = colorsFunc();
 export default class extends React.Component<any, any> {
   target: Object;
 
   render() {
     const config = {
       [Widget.Tooltip]: {
-        color: '#999999',
-        fontColor: '#ffffff',
+        TooltipContainer: {
+          normal: {
+            background: {
+              color: lightGreyColor,
+            },
+          },
+        },
+        TooltipMessage: {
+          normal: {
+            color: dangerColor,
+            fontSize: 16,
+          },
+        },
       },
     };
     const { children, title, action = ['focus'], placement = 'bottom', size } = this.props;
     const getTarget: Function = cmp => (this.target = cmp);
     return (
       <Theme config={config}>
-        <Tooltip placement={placement} title={title} action={action} ref={getTarget} size={size}>
+        <Tooltip
+          placement={placement}
+          title={title}
+          action={action}
+          ref={getTarget}
+          size={size}
+          theme={this.props.themeProps}
+        >
           {children}
         </Tooltip>
       </Theme>
