2e6a6a3fe46235b720b82af77ff58174f3c36113
cuicuiwang <1623330976@qq.com>
2019-07-17 11:26:16 +0800

    修改 date组件主题配置，尚未完成，保存一下，不合并

--- src/widgets/date-picker/styled/utils.js
+++ src/widgets/date-picker/styled/utils.js
@@ -1,8 +1,8 @@
 import colorsFunc from '../../css/stateColor';
-import { px2emcss } from '../../css/units';
+import { px2remcss } from '../../css/units';
 import { modeStyle } from '../utils/booleanUtils';
 export const fontSize = 1.4;
-export const em = px2emcss(fontSize);
+export const em = px2remcss;
 
 export const distance = {
   iconLeft: 10,
@@ -13,29 +13,31 @@ export const DateWrapperPadding = {
   bottom: 44,
 };
 export const borderRadius = 3;
-const { hoverColor, normalColor, disableColor, spiritColor } = colorsFunc();
+const { hoverColor, normalColor, disableColor, spiritColor, lightGreyColor } = colorsFunc();
 export const themeColor = {
   hoverColor,
   normalColor,
   disableColor,
   spiritColor,
+  lightGreyColor,
 };
 export function getThemeProperty(props: Object) {
   const { hasTimeWrapBorder, hasItemNumber = 3 } = props;
   const { isRange, isTime } = modeStyle(props.mode);
   const normalWidth = isRange ? 420 : 200;
   const { width = normalWidth, color, backgroundColor } = props;
-  const newWidth = isRange ? width / 2 + 100 : isTime ? width : width * 1 + 100;
-  const rangeWrapWidth = newWidth * 2;
+  const rangeWrapWidth = width >= 600 ? width : 600;
+  const newWidth = isRange ? rangeWrapWidth / 2 : isTime ? width : width * 1;
   const TimeWrapWidth = isRange && hasTimeWrapBorder ? newWidth - 1 : newWidth;
   const TimeColWidth = TimeWrapWidth / hasItemNumber;
-  const weekTitleWIdth = em((newWidth - DateWrapperPadding.left * 2 - 2) / 7);
+  const weekTitleWidth = (newWidth - DateWrapperPadding.left * 2 - 2) / 7;
+  console.log(props);
   return {
     width: newWidth,
     TimeWrapWidth,
     color,
     backgroundColor,
-    weekTitleWIdth,
+    weekTitleWidth,
     rangeWrapWidth,
     TimeColWidth,
   };
