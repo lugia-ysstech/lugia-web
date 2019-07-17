1222570dfd28e98ee0ff90ec78c297da77b36da7
cuicuiwang <1623330976@qq.com>
2019-07-17 11:23:35 +0800

    修改 date组件主题配置，尚未完成，保存一下，不合并

--- src/widgets/date-picker/styled/styledFooter.js
+++ src/widgets/date-picker/styled/styledFooter.js
@@ -1,42 +1,59 @@
-import styled from 'styled-components';
+import { css } from 'styled-components';
 import { px2emcss } from '../../css/units';
 
 import { themeColor, getDateWrrap, fontSize, em } from './utils';
+import CSSComponent from '@lugia/theme-css-hoc';
 const { normalColor } = themeColor;
 
-export const FooterWrap = styled.div`
-  ${props => (props.showFooter ? 'border-top: 1px solid #ddd;' : '')};
-  padding: ${em(10)} ${props => getDateWrrap(props).left};
-  font-size: ${fontSize}rem;
-  color: ${normalColor};
+export const FooterWrap = CSSComponent({
+  tag: 'div',
+  className: 'FooterWrap',
+  css: css`
+    ${props => (props.showFooter ? 'border-top: 1px solid #ddd;' : '')};
+    padding: ${em(10)} ${props => getDateWrrap(props).left};
+    font-size: ${fontSize}rem;
+    color: ${normalColor};
 
-  &::after {
-    display: block;
-    content: '';
-    clear: both;
-    height: 0;
-  }
-`;
-export const Footer = styled.div`
-  text-align: ${props => (props.showToday ? 'center' : '')};
-`;
-export const FooterBtn = styled.span`
-  ${props => (props.border ? `border-radius:${em(3)};` : '')};
-  ${props => (props.background ? `background:${normalColor};` : '')};
-  font-size: ${em(14)};
-  line-height: ${em(26)};
-  color: ${props => (props.showTime && !props.showTimeButton ? '#ddd' : normalColor)};
-  ${props => getFooterStyle(props).marginSize};
-  cursor: pointer;
-  float: ${props => (props.onOk || props.showTime ? 'right' : props.buttonOptions ? 'left' : '')};
-  ${props => (props.onOk ? `padding:0 ${em(10)};color:#fff;` : '')};
-`;
-export const ExtraFooter = styled.div`
-  font-size: ${em(14)};
-  line-height: ${em(26)};
-  text-align: left;
-  color: #999;
-`;
+    &::after {
+      display: block;
+      content: '';
+      clear: both;
+      height: 0;
+    }
+  `,
+});
+export const Footer = CSSComponent({
+  tag: 'div',
+  className: 'Footer',
+  css: css`
+    text-align: ${props => (props.showToday ? 'center' : '')};
+  `,
+});
+export const FooterBtn = CSSComponent({
+  tag: 'span',
+  className: 'FooterBtn',
+  css: css`
+    ${props => (props.border ? `border-radius:${em(3)};` : '')};
+    ${props => (props.background ? `background:${normalColor};` : '')};
+    font-size: ${em(14)};
+    line-height: ${em(26)};
+    color: ${props => (props.showTime && !props.showTimeButton ? '#ddd' : normalColor)};
+    ${props => getFooterStyle(props).marginSize};
+    cursor: pointer;
+    float: ${props => (props.onOk || props.showTime ? 'right' : props.buttonOptions ? 'left' : '')};
+    ${props => (props.onOk ? `padding:0 ${em(10)};color:#fff;` : '')};
+  `,
+});
+export const ExtraFooter = CSSComponent({
+  tag: 'div',
+  className: 'ExtraFooter',
+  css: css`
+    font-size: ${em(14)};
+    line-height: ${em(26)};
+    text-align: left;
+    color: #999;
+  `,
+});
 const getFooterStyle = props => {
   const { buttonOptions, showToday } = props;
   const direction = buttonOptions ? 'right' : 'left';
@@ -47,7 +64,7 @@ const getFooterStyle = props => {
   };
 };
 export const footerSize = value => {
-  const ems = px2emcss(1.4);
-  const size = ems(value);
+  //const ems = px2emcss(1.4);
+  const size = em(value);
   return size;
 };
