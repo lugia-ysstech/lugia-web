edeb42f996565a9eaec6fafccbdaa18ffe300f97
cuicuiwang <1623330976@qq.com>
2019-07-17 11:25:32 +0800

    修改 date组件主题配置，尚未完成，保存一下，不合并

--- src/widgets/date-picker/styled/styledRangeInput.js
+++ src/widgets/date-picker/styled/styledRangeInput.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { css } from 'styled-components';
 import {
   getInputBorderHoverColor,
   getInputBorderColor,
@@ -6,45 +6,128 @@ import {
   FontSize,
 } from '../../css/input';
 import { getInputBorderRadius } from '../../common/ThemeUtils';
-import { px2emcss } from '../../css/units';
-export const em = px2emcss(FontSize);
+import { px2remcss } from '../../css/units';
+import CSSComponent from '@lugia/theme-css-hoc';
+import { getBorder } from '@lugia/theme-utils';
+import { themeColor } from './utils';
+const { lightGreyColor, normalColor } = themeColor;
+export const em = px2remcss;
 
-export const RangeInputWrap = styled.div`
-  font-size: ${FontSize}rem;
-  display: inline-block;
-  border: 1px solid ${props => getInputBorderColor(props)};
-  ${getInputBorderRadius};
-  width: ${props => em(props.width)};
-  &:hover {
-    border-color: ${props => getInputBorderHoverColor(props)};
-  }
+export const RangeInputWrap = CSSComponent({
+  tag: 'div',
+  className: 'RangeInputWrap',
+  normal: {
+    selectNames: [
+      ['width'],
+      ['height'],
+      ['border'],
+      ['boxShadow'],
+      ['borderRadius'],
+      ['background'],
+    ],
+    defaultTheme: {
+      width: 'auto',
+      border: getBorder({ style: 'solid', width: 1, color: lightGreyColor }),
+    },
+  },
+  hover: {
+    selectNames: [['border'], ['boxShadow'], ['borderRadius'], ['background']],
+    defaultTheme: {
+      border: getBorder({ color: normalColor }),
+    },
+  },
+  active: {
+    selectNames: [['border'], ['boxShadow'], ['borderRadius'], ['background']],
+  },
+  disabled: {
+    selectNames: [['border'], ['boxShadow'], ['borderRadius'], ['background']],
+    defaultTheme: {
+      border: getBorder({ color: lightGreyColor }),
+    },
+  },
+  css: css`
+    font-size: ${FontSize}rem;
+    display: inline-block;
+    ${getInputBorderRadius};
+    ${props => getBackground(props)};
+    transition: all 0.3s;
+  `,
+  option: {
+    hover: true,
+  },
+});
+export const RangeInputInner = CSSComponent({
+  tag: 'span',
+  className: 'RangeInputInner',
+  normal: {
+    selectNames: [],
+    getCSS(themeMate, themeConfig) {
+      console.log(themeMate, themeConfig);
+    },
+  },
+  hover: {
+    selectNames: [],
+  },
+  active: {
+    selectNames: [],
+  },
+  disabled: {
+    selectNames: [],
+  },
+  css: css`
+    & input {
+      border: none;
+      text-align: center;
+      background: transparent;
+    }
 
-  &:focus-within {
-    border-color: ${props => getInputBorderHoverColor(props)};
-  }
+    & input:focus {
+      border: none;
+      box-shadow: none;
+    }
 
-  ${props => getBackground(props)};
-  transition: all 0.3s;
-`;
-export const RangeInputInner = styled.span`
-  & input {
-    border: none;
+    display: block;
+    margin-left: -1px;
+    ${getInputBorderRadius};
+  `,
+});
+export const RangeMiddleSpan = CSSComponent({
+  tag: 'span',
+  className: 'RangeMiddleSpan',
+  normal: {
+    selectNames: [],
+    getCSS(themeMate) {
+      const {
+        width,
+        border: { left: { width: leftWidth = 1 } = {}, right: { width: rightWidth = 1 } = {} },
+      } = ({} = themeMate);
+      return `
+        width:${em((width - leftWidth - rightWidth) * 0.1)}
+         `;
+    },
+  },
+  hover: {
+    selectNames: [],
+    getCSS(themeMate, themeConfig) {
+      const {
+        border: { left: { width: leftWidth = 1 } = {}, right: { width: rightWidth = 1 } = {} },
+      } = ({} = themeMate);
+      const {
+        propsConfig: { width },
+      } = themeConfig;
+      return `
+        width:${em((width - leftWidth - rightWidth) * 0.1)}
+         `;
+    },
+  },
+  active: {
+    selectNames: [],
+  },
+  disabled: {
+    selectNames: [],
+  },
+  css: css`
+    display: inline-block;
     text-align: center;
-    background: transparent;
-  }
-
-  & input:focus {
-    border: none;
-    box-shadow: none;
-  }
-
-  display: block;
-  width: ${props => em(props.width)};
-  margin-left: -1px;
-  ${getInputBorderRadius};
-`;
-export const RangeMiddleSpan = styled.span`
-  display: inline-block;
-  width: ${props => em(props.width)};
-  text-align: center;
-`;
+  `,
+});
