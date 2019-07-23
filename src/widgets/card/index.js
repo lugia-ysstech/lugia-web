dde4b992f7263b0f49a7c1cdf5fe440854a525db
liangguodong00100 <liang_guodong@126.com>
2019-07-23 20:05:54 +0800

    修改     *删除card  operation 属性

--- src/widgets/card/index.js
+++ src/widgets/card/index.js
@@ -331,23 +331,6 @@ const Description = CSSComponent({
     },
   },
 });
-const Operation = CSSComponent({
-  tag: 'div',
-  className: 'CardOperation',
-  normal: {
-    selectNames: [['width'], ['height'], ['color'], ['font'], ['position']],
-    defaultTheme: {
-      font: {
-        weight: 400,
-        size: 14,
-      },
-      position: {
-        right: 20,
-        top: 16,
-      },
-    },
-  },
-});
 
 class Card extends React.Component<CardProps, CardState> {
   static defaultProps = {
@@ -403,7 +386,6 @@ class Card extends React.Component<CardProps, CardState> {
 
     return (
       <CardOutContainer themeProps={resultTheme} type={type} imageOrientation={imageOrientation}>
-        {this.getDetails('operation')}
         {this.getImageContainer()}
         <Content
           themeProps={cardContentTheme}
@@ -456,14 +438,9 @@ class Card extends React.Component<CardProps, CardState> {
     return null;
   }
   getDetails(information: string): React.Node | null {
-    const { operation, title, description, content, children, type } = this.props;
+    const { title, description, content, children, type } = this.props;
     const hasNoContent = !(content && children);
     switch (information) {
-      case 'operation':
-        const operationThemeProps = this.props.getPartOfThemeProps('CardOperation');
-        return hasNoContent && operation ? (
-          <Operation themeProps={operationThemeProps}>{operation}</Operation>
-        ) : null;
       case 'title':
         const titleThemeProps = this.props.getPartOfThemeProps('CardTitle');
         return title ? (
