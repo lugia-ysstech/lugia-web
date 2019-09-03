82f4d58114580869e1710d6debecbda5a91faf47
jelly@pudding <liang_guodong@126.com>
2019-09-03 16:07:26 +0800

    add      *reconstruction pagination.

--- src/widgets/css/pagination.js
+++ src/widgets/css/pagination.js
@@ -1,12 +1,3 @@
-/**
- * Pagination
- * create by guorg
- * @flow
- */
-import { px2emcss } from '../css/units';
-import colorsFunc from '../css/stateColor';
-import styled from 'styled-components';
-
 export type PaginationProps = {
   current?: number,
   defaultCurrent?: number,
@@ -15,10 +6,16 @@ export type PaginationProps = {
   pageSize?: number,
   pageSizeOptions?: string[],
   showQuickJumper?: boolean,
-  showQuickJumper?: boolean,
   showTotal?: (total: number, range: number) => void,
   simple?: boolean,
   total?: number,
   onChange?: (page: number, pageSize: number) => void,
+  getPartOfThemeProps: Function,
 };
-export type PaginationState = {};
+export type MorePageType = 'default' | 'pre' | 'next';
+
+export type PaginationState = {
+  current: number,
+  pageSize: number,
+  showMorePageType: MorePageType,
+};
