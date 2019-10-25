295960cfd542e1ad77e743433481cc9276a882b8
grguang <945455382@qq.com>
2019-10-25 20:57:08 +0800

    Fix:     * defaultTheme normal width 100%;

--- src/widgets/lugiad-core/index.js
+++ src/widgets/lugiad-core/index.js
@@ -23,15 +23,18 @@ function bindHandleEvent(e) {
 }
 
 function themeHandle(id, context, dTh, useSmart) {
+  let cmpTheme = {};
   if (context) {
     const contextTheme = { ...context.getLayout(id).theme };
-    if (useSmart && contextTheme.Container && contextTheme.Container.normal) {
-      contextTheme.Container.normal.width = '100%';
-    }
-    const theme = { ...contextTheme };
-    return theme || {};
+    cmpTheme = { ...contextTheme };
+  } else {
+    cmpTheme = { ...dTh };
   }
-  return dTh || {};
+
+  if (useSmart && cmpTheme.Container && cmpTheme.Container.normal) {
+    cmpTheme.Container.normal.width = '100%';
+  }
+  return cmpTheme || {};
 }
 
 function getData(state, propsName, modelName, fieldName) {
@@ -52,19 +55,19 @@ const pointType2GetCSS: {
 } = {
   leftTop: (point: Point) => {
     const [x, y] = point;
-    return { left: `${x}px`, top: `${y}px`, right: '', bottom: '' };
+    return { left: `${x}%`, top: `${y}%`, right: '', bottom: '' };
   },
   leftBottom: (point: Point) => {
     const [x, y] = point;
-    return { left: `${x}px`, bottom: `${y}px`, right: '', top: '' };
+    return { left: `${x}%`, bottom: `${y}%`, right: '', top: '' };
   },
   rightTop: (point: Point) => {
     const [x, y] = point;
-    return { right: `${x}px`, top: `${y}px`, left: '', bottom: '' };
+    return { right: `${x}%`, top: `${y}%`, left: '', bottom: '' };
   },
   rightBottom: (point: Point) => {
     const [x, y] = point;
-    return { right: `${x}px`, bottom: `${y}px`, left: '', top: '' };
+    return { right: `${x}%`, bottom: `${y}%`, left: '', top: '' };
   },
 };
 
