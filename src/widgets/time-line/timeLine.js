e634e7387c0f23e4814f92cd25c4a8bd47b4bf5f
jelly@pudding <liang_guodong@126.com>
2019-11-26 16:02:25 +0800

    change      *  fix Timeline has no width.

--- src/widgets/time-line/timeLine.js
+++ src/widgets/time-line/timeLine.js
@@ -15,15 +15,13 @@ import TimeLineItem from './timeLineItem';
 import { getAttributeFromObject } from '../common/ObjectUtils';
 import moment from 'moment';
 import CSSComponent, { css } from '@lugia/theme-css-hoc';
+import { deepMerge } from '@lugia/object-utils';
 
 const OutContainer = CSSComponent({
   tag: 'div',
   className: 'TimeLineContainer',
   normal: {
     selectNames: [['width'], ['height'], ['padding'], ['margin']],
-    defaultTheme: {
-      width: 200,
-    },
   },
 });
 
@@ -65,15 +63,25 @@ class TimeLine extends Component<TimeLineProps, TimeLineState> {
     defaultData,
   };
   static displayName = Widget.TimeLine;
-
+  state = { _renderWidth: false };
   constructor(props: TimeLineProps) {
     super(props);
   }
 
   static getDerivedStateFromProps(props: TimeLineProps, state: TimeLineState) {}
-
+  getContainerHeight() {
+    if (this.cmpWidth) {
+      const theWidth = this.cmpWidth;
+      return {
+        width: theWidth,
+      };
+    }
+  }
   render() {
-    const theThemeProps = this.props.getPartOfThemeProps('TimeLineContainer', {});
+    const theThemeProps = deepMerge(
+      { themeConfig: { normal: this.getContainerHeight() } },
+      this.props.getPartOfThemeProps('TimeLineContainer')
+    );
     return <OutContainer themeProps={theThemeProps}>{this.getChildren()}</OutContainer>;
   }
 
@@ -87,20 +95,32 @@ class TimeLine extends Component<TimeLineProps, TimeLineState> {
 
   getMapChildren() {
     const { data, defaultData, children } = this.props;
-    return data
+    const finalData = data
       ? this.data2Item(data)
       : Array.isArray(children) && children.length > 0
       ? React.Children.map(children, (child, i) => {
           return React.cloneElement(child, this.getItemProps(child, i));
         })
       : this.data2Item(defaultData);
+    this.childrenLength = finalData.length;
+    return finalData;
   }
 
   getItemProps(child: Object, i: number) {
-    const { reverse, mode, pendingDot, pending, data, children, defaultData } = this.props;
+    const {
+      reverse,
+      mode,
+      pendingDot,
+      pending,
+      data,
+      children,
+      defaultData,
+      getPartOfThemeHocProps,
+    } = this.props;
     const size = data ? data.length : children ? children.length : defaultData.length;
     const getDirection = this.getDirection(mode);
     return {
+      ...getPartOfThemeHocProps('TimeLineItem'),
       isLast: this.isLast(i, size, reverse),
       direction: getDirection(i),
       pending,
@@ -122,8 +142,48 @@ class TimeLine extends Component<TimeLineProps, TimeLineState> {
         'type',
         getAttributeFromObject(child.props, 'type', 'icon')
       ),
+      mode,
+      getChildDirectionAndWidth: this.getChildDirectionAndWidth,
+      _leftMaxWidth: this.leftChildMaxWidth,
     };
   }
+  handleCmpWidth(widthArray: Array) {
+    const { mode } = this.props;
+    if (mode === 'alternate') {
+      this.cmpWidth = widthArray[0] + widthArray[1];
+      for (let i = 0; i < widthArray.length; i++) {
+        if (widthArray[i + 1] + widthArray[i] > widthArray[i] * 2) {
+          this.cmpWidth = widthArray[i + 1] + widthArray[i];
+        }
+      }
+    } else {
+      this.cmpWidth = Math.max.apply(null, widthArray);
+    }
+    this.cmpWidth += 20;
+  }
+  widthArray = [];
+  childArray = [];
+  leftChildMaxWidth = 0;
+  getChildDirectionAndWidth = (childObj: Object) => {
+    const { width, direction } = childObj;
+    this.widthArray.push(width);
+    this.childArray.push(childObj);
+    if (this.childrenLength === this.widthArray.length) {
+      this.handleCmpWidth(this.widthArray);
+      this.leftChildMaxWidth = this.childArray[1].width;
+      for (let i = 0; i < this.childArray.length; i++) {
+        if (
+          this.childArray[i].direction === 'right' &&
+          this.childArray[i + 1] &&
+          this.childArray[i + 1].width > this.childArray[i].width
+        ) {
+          this.leftChildMaxWidth = this.childArray[i + 1].width;
+        }
+      }
+    }
+    console.log(this.leftChildMaxWidth, 111111111);
+    this.setState({ _renderWidth: true });
+  };
 
   data2Item(data: Array<Object>) {
     return data.map((child, i) => {
