9ddc7d71cd06413d4c56a667959e9c79565e1408
cuicuiwang <1623330976@qq.com>
2019-12-12 18:33:32 +0800

    新增：增加基本元素组件

---
+++ src/widgets/basicElements/styled.js
@@ -0,0 +1,22 @@
+import { css } from 'styled-components';
+import CSSProvider from '../theme/CSSProvider';
+
+export const Box = CSSProvider({
+  tag: 'span',
+  className: 'Container',
+  normal: {
+    selectNames: [
+      ['width'],
+      ['height'],
+      ['border'],
+      ['borderRadius'],
+      ['boxShadow'],
+      ['margin'],
+      ['background'],
+      ['opacity'],
+    ],
+  },
+  css: css`
+    display: inline-block;
+  `,
+});
