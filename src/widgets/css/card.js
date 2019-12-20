97f5fca47929f6ffd56589893c931bb28cbc09f7
jelly@pudding <liang_guodong@126.com>
2019-12-20 13:30:22 +0800

    Add       *  Card add props showTipLine that is show "TitleBottomLine" while Card type is "tip.

--- src/widgets/css/card.js
+++ src/widgets/css/card.js
@@ -26,5 +26,6 @@ export type CardProps = {
   getPartOfThemeProps: Function,
   getPartOfThemeHocProps: Function,
   getPartOfThemeConfig: Function,
+  showTipLine?: boolean,
 };
 export type CardState = {};
