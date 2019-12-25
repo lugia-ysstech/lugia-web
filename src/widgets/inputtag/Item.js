54a3d15fe0aab7b583c06680c39eeb11a91a6688
szfeng <360998950@qq.com>
2019-12-25 21:29:01 +0800

    修复： 1、选择器无法百分百自适应 2、选择器外盒没有禁用状态

--- src/widgets/inputtag/Item.js
+++ src/widgets/inputtag/Item.js
@@ -7,8 +7,11 @@
 import React from 'react';
 import Widget from '../consts/index';
 import ThemeHoc from '@lugia/theme-hoc';
-
-import { ItemContainer, ItemText, CloseButtonWrap, ItemWrap, CloseButton } from '../css/inputtag';
+import Icon from '../icon';
+import { deepMerge } from '@lugia/object-utils';
+import { ItemContainer, ItemText, ItemWrap, FontSize } from '../css/inputtag';
+import colorsFunc from '../css/stateColor';
+export const { mediumGreyColor } = colorsFunc();
 
 type ItemProps = {
   className?: string,
@@ -17,6 +20,7 @@ type ItemProps = {
   onClick?: Function,
   onCloseClick?: Function,
   getPartOfThemeProps: Function,
+  getPartOfThemeHocProps: Function,
 };
 
 type ItemState = {};
@@ -27,10 +31,9 @@ class ItemTag extends React.Component<ItemProps, ItemState> {
   static displayName = Widget.InputTagItem;
   render() {
     const { className, closeable = true, onClick, onCloseClick, getPartOfThemeProps } = this.props;
-
     const TagWrapThemeProps = getPartOfThemeProps('TagWrap');
     return (
-      <ItemWrap themeProps={TagWrapThemeProps}>
+      <ItemWrap>
         <ItemContainer
           ref={c => (this.item = c)}
           themeProps={TagWrapThemeProps}
@@ -40,17 +43,43 @@ class ItemTag extends React.Component<ItemProps, ItemState> {
         >
           <ItemText themeProps={TagWrapThemeProps}>{this.props.children}</ItemText>
           {closeable ? (
-            <CloseButtonWrap themeProps={getPartOfThemeProps('TagIcon')}>
-              <CloseButton
-                themeProps={TagWrapThemeProps}
-                iconClass="lugia-icon-reminder_close_circle"
-                onClick={onCloseClick}
-              />
-            </CloseButtonWrap>
+            <Icon
+              iconClass="lugia-icon-reminder_close_circle"
+              {...this.getIconTheme()}
+              singleTheme
+              onClick={onCloseClick}
+            />
           ) : null}
         </ItemContainer>
       </ItemWrap>
     );
+  }
+
+  getIconTheme() {
+    const { viewClass, theme } = this.props.getPartOfThemeHocProps('TagIcon');
+    const defaultIconTheme = {
+      normal: {
+        color: mediumGreyColor,
+        font: {
+          size: FontSize,
+        },
+        getCSS: () => {
+          return `
+          transition: all 0.3s
+          `;
+        },
+      },
+    };
+
+    return {
+      viewClass,
+      theme: deepMerge(
+        {
+          [viewClass]: { ...defaultIconTheme },
+        },
+        theme
+      ),
+    };
   }
 
   componentDidMount() {
