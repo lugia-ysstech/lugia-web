98a2d5fab80801daf0109dbd877130644dada097
szfeng <360998950@qq.com>
2020-01-02 14:09:42 +0800

    修改：面包屑组件添加前后缀图标

--- src/widgets/breadcrumb/breadcrumbItem.js
+++ src/widgets/breadcrumb/breadcrumbItem.js
@@ -7,7 +7,8 @@
 import * as React from 'react';
 import ThemeHoc from '@lugia/theme-hoc';
 import { ALink, CommonSpan, SeparatorSpan, ItemWrap, FlexBox } from '../css/breadcrumb';
-
+import Icon from '../icon';
+import { deepMerge } from '@lugia/object-utils';
 export type BreadcrumbItemProps = {
   separator?: string | React.Element<any>,
   href?: string,
@@ -19,11 +20,82 @@ export type BreadcrumbItemProps = {
   getPartOfThemeProps: Function,
   index: number,
   count: number,
+  icons: Object,
 };
 
 class BreadcrumbItem extends React.Component<BreadcrumbItemProps, any> {
   static defaultProps = {
     separator: '/',
+  };
+
+  getPrefixIcon(channel: Object) {
+    const { icons = {} } = this.props;
+    if (!icons) {
+      return null;
+    }
+    const { prefixIconClass } = icons;
+    if (!prefixIconClass) {
+      return null;
+    }
+    const { viewClass, theme } = this.getIconTheme('PrefixIcon');
+
+    return (
+      <Icon
+        iconClass={prefixIconClass}
+        lugiaConsumers={channel.consumer}
+        singleTheme
+        viewClass={viewClass}
+        theme={theme}
+      />
+    );
+  }
+
+  getSuffixIcon(channel: Object) {
+    const { icons = {} } = this.props;
+    if (!icons) {
+      return null;
+    }
+    const { suffixIconClass } = icons;
+
+    if (!suffixIconClass) {
+      return null;
+    }
+
+    const { viewClass, theme } = this.getIconTheme('SuffixIcon');
+
+    return (
+      <Icon
+        iconClass={suffixIconClass}
+        lugiaConsumers={channel.consumer}
+        singleTheme
+        viewClass={viewClass}
+        theme={theme}
+      />
+    );
+  }
+
+  getIconTheme = (iconType: string) => {
+    const { viewClass, theme } = this.props.getPartOfThemeHocProps(iconType);
+    const marginLeft = iconType === 'SuffixIcon' ? 3 : 0;
+    const marginRight = iconType === 'PrefixIcon' ? 3 : 0;
+    const defaultTheme = {
+      normal: {
+        margin: {
+          left: marginLeft,
+          right: marginRight,
+        },
+      },
+    };
+
+    return {
+      viewClass,
+      theme: deepMerge(
+        {
+          [viewClass]: { ...defaultTheme },
+        },
+        theme
+      ),
+    };
   };
 
   render() {
@@ -32,6 +104,8 @@ class BreadcrumbItem extends React.Component<BreadcrumbItemProps, any> {
     if ('href' in this.props) {
       Link = ALink;
     }
+    const channel = this.props.createEventChannel(['active', 'hover']);
+
     return (
       <ItemWrap themeProps={getPartOfThemeProps('ItemWrap', { selector: { index, count } })}>
         <FlexBox themeProps={getPartOfThemeProps('ItemWrap', { selector: { index, count } })}>
@@ -42,7 +116,9 @@ class BreadcrumbItem extends React.Component<BreadcrumbItemProps, any> {
               props: { isLastItem },
             })}
           >
+            {this.getPrefixIcon(channel)}
             {children}
+            {this.getSuffixIcon(channel)}
           </Link>
           <SeparatorSpan
             themeProps={getPartOfThemeProps('Separator', {
