d6e4ed0cc42404963639afdfd937f8f18ece56bf
cuicuiwang <1623330976@qq.com>
2020-01-06 19:17:14 +0800

    修改：样式修改

--- src/widgets/slider/styled.js
+++ src/widgets/slider/styled.js
@@ -62,7 +62,6 @@ export const SliderBigBox = CSSComponent({
     display: inline-block;
     box-sizing: border-box;
     vertical-align: top;
-    overflow: hidden;
   `,
 });
 export const SliderBox = StaticComponent({
