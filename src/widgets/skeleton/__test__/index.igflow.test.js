0ac9f1099e6b84810ff73b66020883d43c8b7d82
szfeng <360998950@qq.com>
2020-01-09 14:01:27 +0800

    删除无用代码

--- src/widgets/skeleton/__test__/index.igflow.test.js
+++ src/widgets/skeleton/__test__/index.igflow.test.js
@@ -6,25 +6,6 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Skeleton', () => {
-  // 错误用例
-  // it('getParagraphWidth', () => {
-  //   expect(Skeleton.prototype.getParagraphWidth('500', 3)).toEqual({ 2: '500' });
-  //   expect(Skeleton.prototype.getParagraphWidth(500, 3)).toEqual({ 2: 500 });
-  //   expect(Skeleton.prototype.getParagraphWidth(undefined, 3)).toEqual({ 2: undefined });
-  //   expect(Skeleton.prototype.getParagraphWidth(null, 3)).toEqual({ 2: null });
-  //   expect(Skeleton.prototype.getParagraphWidth('', 3)).toEqual({ 2: '' });
-  //   expect(Skeleton.prototype.getParagraphWidth({}, 3)).toEqual({ 2: {} });
-  //   expect(Skeleton.prototype.getParagraphWidth([], 3)).toEqual({ 2: [] });
-
-  //   expect(Skeleton.prototype.getParagraphWidth([200], 3)).toEqual([200]);
-  //   expect(Skeleton.prototype.getParagraphWidth(['200', '300'], 3)).toEqual(['200', '300']);
-  //   expect(Skeleton.prototype.getParagraphWidth(['200', '300', '400'], 3)).toEqual([
-  //     '200',
-  //     '300',
-  //     '400',
-  //   ]);
-  // });
-
   it('getParagraphCount', () => {
     expect(Skeleton.prototype.getParagraphCount(true)).toEqual(3);
     expect(Skeleton.prototype.getParagraphCount(false)).toEqual(3);
