645f5b686e94d8fdcb4d6422fc747671ee8412a3
jelly@pudding <liang_guodong@126.com>
2020-01-19 09:54:07 +0800

    Update     *  Update test snapshot

--- src/widgets/card/__tests__/index.test.js
+++ src/widgets/card/__tests__/index.test.js
@@ -7,12 +7,12 @@
 import React from 'react';
 
 import 'jest-styled-components';
-import Wrapper from '../demo';
 import renderer from 'react-test-renderer';
+import { CardDemo } from '../demo';
 
 describe('Card', () => {
   it('css', () => {
-    const Target = <Wrapper />;
+    const Target = <CardDemo />;
     expect(renderer.create(Target).toJSON()).toMatchSnapshot();
   });
 });
