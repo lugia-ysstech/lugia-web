f6c737aae054687bebf82af905fc56c211e9304c
cuicuiwang <1623330976@qq.com>
2020-01-19 15:31:28 +0800

    修改：测试用例更新默认值

--- src/widgets/css/__test__/stateColor.test.js
+++ src/widgets/css/__test__/stateColor.test.js
@@ -19,26 +19,27 @@ const DefaultCommonStyle = {
   successColor: '#56c22d', //成功色
   warningColor: '#f8ac30', //警告色
   dangerColor: '#f22735', //危险色
-  blackColor: '#333333', //黑色
-  darkGreyColor: '#666666', //深灰色
-  mediumGreyColor: '#999999', //中灰色
-  lightGreyColor: '#cccccc', //浅灰色
-  superLightColor: '#e8e8e8', //超浅色
+  blackColor: '#333333', //标题色
+  darkGreyColor: '#666666', //正文色
+  disableTextColor: '#cccccc', //文字禁止色
+  mediumGreyColor: '#999999', //标注色
+  lightGreyColor: '#cccccc', //水印色
+  superLightColor: '#e8e8e8', //辅助色
   disableColor: '#f2f2f2', //禁止色
-  defaultColor: '#ffffff', //默认白色色
+  defaultColor: '#ffffff', //默认色
   borderColor: '#cccccc', //边框色
   borderDisableColor: '#e8e8e8', //边框禁止色
-  borderSize: '1px', //边框大小
-  boxShadowOpacity: '20%', //边框透明度
-  hShadow: '0', //边框水平偏移值（X）
-  vShadow: '0', //边框竖直偏移值（Y）
-  shadowSpread: 6, //边框大小
+  borderSize: 1, //边框大小
+  boxShadowOpacity: 0.2, //阴影透明度
+  hShadow: '0', //阴影水平偏移值（X）
+  vShadow: '0', //阴影竖直偏移值（Y）
+  shadowSpread: 6, //阴影扩散大小
   borderRadius: 4, //圆角通用
   circleBorderRadius: '50%', //圆角-特殊
   transitionTime: '0.3s', //过渡时间
   rulesColor: '#fff', //分割线颜色
-  rulesSize: '1px', //分割线大小
-  rulesOpacity: '40%', //分割线透明度
+  rulesSize: 1, //分割线大小
+  rulesOpacity: 0.4, //分割线透明度
   padding: 10, //元素左右padding
   paddingToText: 6, //文字到元素的距离
   marginToSameElement: 10, //同级元素与元素之间距离
