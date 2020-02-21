7495dea9a47f1833d7f1aaa308149f30493d7961
cuicuiwang <1623330976@qq.com>
2020-02-21 17:41:27 +0800

    update：增加彩色图标

--- src/widgets/icon/Svg.js
+++ src/widgets/icon/Svg.js
@@ -3,7 +3,7 @@ import symbolSvg from '../css/font/lugia-symbol-icon.symbol.svg';
 export default function Svg(props) {
   const { iconClass = 'lugia-symbol-icon-symbol_autohotkey' } = props;
   return (
-    <svg className="icon" ariaHidden="true" viewBox="0 0 48 48">
+    <svg className="icon" ariaHidden="true">
       <use href={`${symbolSvg}#${iconClass}`} />
     </svg>
   );
