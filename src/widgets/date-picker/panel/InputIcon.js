dc22767969ff0fb238018dd5d776f7a3b660475a
cuicuiwang <1623330976@qq.com>
2020-03-06 16:11:09 +0800

    update：日期组件后缀图标闪烁

--- src/widgets/date-picker/panel/InputIcon.js
+++ src/widgets/date-picker/panel/InputIcon.js
@@ -1,65 +1,15 @@
 import React from 'react';
 import Icon from '../../icon';
-import styled from 'styled-components';
-import Theme from '../../theme';
-import Widget from '../../consts/index';
-const IconWrap = styled.div`
-  &:hover {
-    & > span:first-child {
-      display: none;
-    }
-
-    & > span :last-child {
-      display: block;
-    }
-  }
-  & > span:last-child {
-    display: none;
-  }
-`;
+import Suffix from './Suffix';
 
 export default function getDateIcon(props) {
-  const { suffix, prefix = 'lugia-icon-financial_date', onClear, value, clearButtonTheme } = props;
-  const clearBtn = (
-    <Theme
-      config={{
-        [Widget.Icon]: {
-          Icon: {
-            disabled: {
-              color: '#ddd',
-            },
-            ...clearButtonTheme.themeConfig,
-          },
-        },
-      }}
-    >
-      <Icon
-        iconClass={'lugia-icon-reminder_close'}
-        onClick={e => {
-          onClear(e);
-        }}
-      />
-    </Theme>
-  );
-  const suffixIcon =
-    suffix && typeof suffix === 'string' ? (
-      <IconWrap>
-        <span>
-          <Icon iconClass={suffix} />
-        </span>
-        <span>{clearBtn}</span>
-      </IconWrap>
-    ) : value ? (
-      clearBtn
-    ) : (
-      <i />
-    );
+  const { suffix, prefix = 'lugia-icon-financial_date' } = props;
 
   const prefixIcon =
     !suffix && prefix && typeof prefix === 'string' ? <Icon iconClass={prefix} /> : <i />;
 
   return {
-    suffixIcon: { suffix: suffixIcon },
+    suffixIcon: { suffix: <Suffix {...props} /> },
     prefixIcon: { prefix: prefixIcon },
   };
 }
