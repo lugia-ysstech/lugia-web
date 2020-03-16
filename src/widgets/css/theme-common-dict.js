7a0bf44a4b48df040ccf4078f87e9fd6bfb69788
grguang <945455382@qq.com>
2020-03-16 15:28:53 +0800

    Update:     * theme-common-dict add ruleColor/buttonRuleColor;

--- src/widgets/css/theme-common-dict.js
+++ src/widgets/css/theme-common-dict.js
@@ -5,23 +5,102 @@
  * @flow
  */
 import { getDict } from '@lugia/dict';
+import PublicValue from './public-value';
+import changeColor from './utilsColor';
 
 const dict = getDict('@lugia/lugia-web');
 
 const NameSpace = 'Default';
+const reduceS = 45;
+const bgReduceA = 5;
 /**
  * 加载lugia-web的默认主题
  */
-dict.load(NameSpace, {
-  themeColor: '#4d63ff', //主题色
-});
+dict.load(NameSpace, PublicValue);
+load(PublicValue);
 
 /**
  * 变更发布（生产）环境下的公共值
  * @param value 新的值
  */
 export function load(value: Object) {
-  dict.load(NameSpace, value);
+  if (value) {
+    const ruleColor = getRuleColor(value);
+    const buttonRuleColor = getButtonRuleColor(value);
+    dict.load(NameSpace, { ...value, ...ruleColor, ...buttonRuleColor });
+  }
+}
+
+function getRuleColor(value: Object): Object {
+  const { themeColor } = value;
+  const ruleColor = {
+    normalColor: getReduceColor(themeColor, {}, 'color'),
+    hoverColor: getReduceColor(themeColor, { reduceS: 20 }, 'color'),
+    mouseDownColor: getReduceColor(themeColor, { reduceS: 0, reduceB: 20 }, 'color'),
+    disabledColor: getReduceColor(themeColor, { reduceS: 45 }, 'color'),
+    spiritColor: getReduceColor(themeColor, { reduceS: 0, reduceB: 0, reduceA: 5 }, 'rgba'),
+    disabledSpiritBackgroundColor: getReduceColor(
+      themeColor,
+      { reduceS: 0, reduceB: 0, reduceA: 1.5 },
+      'rgba'
+    ),
+    disabledSpiritFontAndBorderColor: getReduceColor(
+      themeColor,
+      { reduceS: 0, reduceB: 0, reduceA: 30 },
+      'rgba'
+    ),
+  };
+
+  return ruleColor;
+}
+
+function getButtonRuleColor(value: Object): Object {
+  const { themeColor, successColor, warningColor, dangerColor } = value;
+  const buttonRuleColor = {
+    defaultColorReduceS: getReduceColor('#333', { reduceS }),
+    themeColorReduceS: getReduceColor(themeColor, { reduceS }),
+    successColorReduceS: getReduceColor(successColor, { reduceS }),
+    warningColorReduceS: getReduceColor(warningColor, { reduceS }),
+    dangerColorReduceS: getReduceColor(dangerColor, { reduceS }),
+    themeColorReduceA: getReduceColor(
+      themeColor,
+      { reduceS: 0, reduceB: 0, reduceA: bgReduceA },
+      'rgba'
+    ),
+    successColorReduceA: getReduceColor(
+      successColor,
+      { reduceS: 0, reduceB: 0, reduceA: bgReduceA },
+      'rgba'
+    ),
+    warningColorReduceA: getReduceColor(
+      warningColor,
+      { reduceS: 0, reduceB: 0, reduceA: bgReduceA },
+      'rgba'
+    ),
+    dangerColorReduceA: getReduceColor(
+      dangerColor,
+      { reduceS: 0, reduceB: 0, reduceA: bgReduceA },
+      'rgba'
+    ),
+    mouseDownSuccessColor: getReduceColor(successColor, { reduceS: 0, reduceB: 20 }, 'color'),
+    mouseDownWarningColor: getReduceColor(warningColor, { reduceS: 0, reduceB: 20 }, 'color'),
+    mouseDownDangerColor: getReduceColor(dangerColor, { reduceS: 0, reduceB: 20 }, 'color'),
+  };
+
+  return buttonRuleColor;
+}
+
+function getReduceColor(
+  targetColor: string,
+  reduce: { reduceS: number, reduceB?: number, reduceA?: number },
+  resultStr: 'color' | 'rgba' = 'color'
+) {
+  if (targetColor) {
+    const { reduceS, reduceB, reduceA } = reduce;
+
+    return changeColor(targetColor, reduceS, reduceB, reduceA)[resultStr];
+  }
+  return undefined;
 }
 
 const DevNameSpace = 'DevNameSpace';
