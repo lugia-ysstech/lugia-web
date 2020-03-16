7d6f947ea2458c2a4941fd484d4ff8ba3aec504c
zenjava <138962816@qq.com>
2020-03-16 16:26:55 +0800

    扩展功能：   * Modal组件增加一个Modal.createShowModal的方法，用来构建出一个可以快速弹出对话框的方法。

--- src/widgets/modal/index.js
+++ src/widgets/modal/index.js
@@ -6,11 +6,12 @@
  */
 import Modal from './modal';
 import { quickcall } from './quickcall';
+import createShowModal from './create-show-modal';
 
 Modal.confirm = quickcall('confirm');
 Modal.info = quickcall('info');
 Modal.success = quickcall('success');
 Modal.error = quickcall('error');
 Modal.warning = quickcall('warning');
-
+Modal.createShowModal = createShowModal;
 export default Modal;
