5bfe65dd56dc0a6628b7ec23036cadf84e252ea7
jelly@pudding <liang_guodong@126.com>
2020-04-12 21:03:16 +0800

    Update     *  update  input add validate

--- src/widgets/input/validateCSS.js
+++ src/widgets/input/validateCSS.js
@@ -1,22 +1,16 @@
 import React from 'react';
-
 import CSSComponent, { css, StaticComponent } from '@lugia/theme-css-hoc';
-import { isValidateError } from '../css/input';
-import { getBorder, getBoxShadow } from '@lugia/theme-utils';
-import colorsFunc from '../css/stateColor';
+import { isValidateError } from '../css/validateHoc';
 import { units } from '@lugia/css';
 import get from '../css/theme-common-dict';
 const { px2remcss } = units;
-const { shadowSpread, hShadow, vShadow, borderSize } = colorsFunc();
-const borderColor = '$lugia-dict.@lugia/lugia-web.borderColor';
 const dangerColor = '$lugia-dict.@lugia/lugia-web.dangerColor';
-const dangerHoverColor = '$lugia-dict.@lugia/lugia-web.dangerHoverColor';
-const dangerActiveColor = '$lugia-dict.@lugia/lugia-web.dangerActiveColor';
 
 function getTipShowCSS(themeProps: Object) {
   const { propsConfig } = themeProps;
-  const { validateStatus, _isTipMaskVisible = true } = propsConfig;
-  const theVisibility = isValidateError(validateStatus) && _isTipMaskVisible ? 'visible' : 'hidden';
+  const { validateStatus, _isValidateVisible = true } = propsConfig;
+  const theVisibility =
+    isValidateError(validateStatus) && _isValidateVisible ? 'visible' : 'hidden';
   return `visibility:${theVisibility}`;
 }
 
@@ -54,70 +48,25 @@ export const InnerTipText: Object = CSSComponent({
     defaultTheme: {
       color: dangerColor,
     },
-  },
-  css: css`
-    min-width: ${px2remcss(30)};
-    background-color: ${get('defaultColor')};
-  `,
-});
-export const InnerTip: Object = CSSComponent({
-  tag: 'span',
-  className: 'InnerTip',
-  normal: {
-    selectNames: [],
     getCSS(themeMeta: Object, themeProps: Object) {
       return getTipShowCSS(themeProps);
     },
   },
   css: css`
     min-width: ${px2remcss(30)};
+    background-color: ${get('defaultColor')};
     position: absolute;
     right: ${px2remcss(32)};
     top: 50%;
     transform: translateY(-50%);
-    display: inline-flex;
-  `,
-});
-export const InnerTipMask: Object = StaticComponent({
-  tag: 'div',
-  className: 'InnerTipMask',
-  css: css`
-    width: ${px2remcss(15)};
+    box-shadow: ${px2remcss(-14)} 0 ${px2remcss(6)} 0 ${get('defaultColor')};
   `,
 });
 export const FatherContainer: Object = StaticComponent({
-  tag: 'div',
+  tag: 'span',
   className: 'FatherContainer',
   css: css`
     position: relative;
     height: 100%;
   `,
 });
-
-export const defaultBorderThemeProps = {
-  themeConfig: {
-    normal: {
-      border: getBorder({ color: dangerColor, width: borderSize, style: 'solid' }),
-      boxShadow: getBoxShadow(
-        `${hShadow}px ${vShadow}px ${shadowSpread}px ${get('inputDangerColor')}`
-      ),
-    },
-    hover: {
-      border: getBorder({ color: dangerHoverColor, width: borderSize, style: 'solid' }),
-    },
-    active: {
-      border: getBorder({ color: dangerActiveColor, width: borderSize, style: 'solid' }),
-      boxShadow: getBoxShadow(`${hShadow}px ${vShadow}px ${shadowSpread}px ${dangerActiveColor}`),
-    },
-    disable: {
-      border: getBorder({ color: borderColor, width: borderSize, style: 'solid' }),
-    },
-  },
-};
-export const defaultFontColorThemeProps = {
-  themeConfig: {
-    normal: {
-      color: dangerColor,
-    },
-  },
-};
