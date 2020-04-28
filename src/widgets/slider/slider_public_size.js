34543dadc5d8b5524c30a2ccc6368a63149a521f
Shine_Lee <wangyi_lixian@163.com>
2020-04-28 13:59:36 +0800

    Fix: set entrance

--- src/widgets/slider/slider_public_size.js
+++ src/widgets/slider/slider_public_size.js
@@ -1,5 +1,8 @@
+import get from '../css/theme-common-dict';
+
 export const iconStyles = {
   fontSizeNormal: 20,
+  marginNormal: 8,
 };
 export const btnWidthNormal = 16;
 export const rangeHeightNormal = 6;
@@ -7,3 +10,6 @@ export const rangeWidthNormal = '100%';
 export const dotStyles = {
   distanceForSlider: 25,
 };
+export const horizontalPadding = get('padding');
+export const marginToPeerElementForY = get('marginToPeerElementForY');
+export const mFontSize = get('mFontSize');
