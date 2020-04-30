64e71d898b5a961214a2b07101eea89a1f71c138
jelly@pudding <liang_guodong@126.com>
2020-04-30 09:58:09 +0800

    Update     * update pagination publicValue

--- src/widgets/css/pagination.js
+++ src/widgets/css/pagination.js
@@ -1,3 +1,7 @@
+import get from '../css/theme-common-dict';
+export type MorePageType = 'default' | 'pre' | 'next';
+export type AlignType = 'Left' | 'Right';
+export type SizeType = 'small' | 'default' | 'large';
 export type PaginationProps = {
   current?: number,
   defaultCurrent?: number,
@@ -20,12 +24,37 @@ export type PaginationProps = {
   blockList?: string[],
   align?: AlignType,
   showTotalData?: boolean,
+  size: SizeType,
 };
-export type MorePageType = 'default' | 'pre' | 'next';
-export type AlignType = 'Left' | 'Right';
 
 export type PaginationState = {
   current: number,
   pageSize: number,
   showMorePageType: MorePageType,
 };
+
+function getSize(size: SizeType) {
+  return size === 'small' ? 12 : 14;
+}
+function getIconSize(size: SizeType) {
+  return size === 'small' ? get('xsFontSize') : get('sFontSize');
+}
+export function getThemeFontSize(themeMeta, themeProps, iconType?: boolean) {
+  const { propsConfig: { size } = {} } = themeProps;
+  const { fontSize, font: { size: innerFontSize } = {} } = themeMeta;
+  const theSize = innerFontSize || fontSize || iconType ? getIconSize(size) : getSize(size);
+  return { fontSize: theSize };
+}
+export function getPaginationSize(size: SizeType) {
+  return size === 'small'
+    ? get('smallSize')
+    : size === 'large'
+    ? get('largeSize')
+    : get('normalSize');
+}
+export function getPaginationItemStyle(themeMeta, themeProps) {
+  const { width, height } = themeMeta;
+  const { propsConfig: { size } = {} } = themeProps;
+  const theHeight = height || getPaginationSize(size);
+  return { width: width || getPaginationSize(size), height: theHeight, lineHeight: theHeight };
+}
