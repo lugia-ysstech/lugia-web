424c5444523d0d241214f338c3fcaf88a3fe2654
SleepyAries <410100498@qq.com>
2020-05-08 17:21:23 +0800

    Update:     add defaultTheme

--- src/widgets/new-table/utils.js
+++ src/widgets/new-table/utils.js
@@ -1,19 +1,27 @@
 //@flow
 import { deepMerge } from '@lugia/object-utils';
 
-export const getEditDivTheme = (props, isHead, propsConfig, defaultTheme) => {
+export const getEditDivTheme = (
+  props: Object,
+  isHead: boolean,
+  propsConfig: Object,
+  defaultTheme: Object
+) => {
   let editDivTheme = deepMerge(
     props.getPartOfThemeProps('EditTarget', {
       props: { ...propsConfig },
     }),
     defaultTheme
   );
+  console.log('isHead', isHead, 'propsConfig', propsConfig);
   if (isHead) {
     const tableTheme = props.getPartOfThemeProps('Table');
     const { themeConfig: { Th_Td } = {} } = tableTheme;
     const editingTheme = { themeConfig: { ...Th_Td } };
     editDivTheme = deepMerge(editDivTheme, editingTheme);
+    console.log('tableTheme', tableTheme, 'editingTheme', editingTheme);
   }
+  console.log('editDivTheme', editDivTheme);
   return editDivTheme;
 };
 
