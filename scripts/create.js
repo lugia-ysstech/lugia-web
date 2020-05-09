974e867896cab9a03db5b21a90159e401f5e7830
grguang <945455382@qq.com>
2020-05-09 10:59:09 +0800

    Update:     * designInfo script;

--- scripts/create.js
+++ scripts/create.js
@@ -6,6 +6,8 @@
  */
 const createDesignInfo = require('@lugia/devtools-material').createDesignInfo;
 const path = require('path');
+const ensureFileSync = require('fs-extra').ensureFileSync;
+const writeFileSync = require('fs-extra').writeFileSync;
 
 const tartgetPath = path.join(__dirname, '../src/widgets');
 const Invalid = [
@@ -40,4 +42,13 @@ const Invalid = [
   'collapse'
 ];
 
-createDesignInfo(tartgetPath, Invalid);
+const loadStr = "import { load } from './css/theme-common-dict.js';\n" + 'export { load };';
+
+async function createDesignInfoFile() {
+  const designInfoStr = await createDesignInfo(tartgetPath, Invalid, { outFile: 'string' });
+  const designInfoPath = path.join(tartgetPath, 'designInfo.js');
+  ensureFileSync(designInfoPath);
+  writeFileSync(designInfoPath, designInfoStr + loadStr);
+}
+
+createDesignInfoFile();
