3822aa5b71bf3fafbb8193fab7ae1f74fa406f9d
jelly@pudding <liang_guodong@126.com>
2020-05-15 10:27:36 +0800

    Update     * Update Timeline publicValue

--- src/widgets/time-line/demo.js
+++ src/widgets/time-line/demo.js
@@ -46,21 +46,6 @@ const Wrapper = styled.div`
   margin-left: 100px;
   display: inline-block;
 `;
-const view = {
-  [Widget.TimeLineItem]: {
-    TimeLineItemTip: {
-      TooltipContent: { normal: { background: { color: 'pink' } } },
-      TooltipTitle: { normal: { color: 'green' } },
-      TooltipDescription: { normal: { color: 'red' } },
-    },
-    TimeLineIcon: {
-      normal: {
-        color: 'red',
-        fontSize: 16,
-      },
-    },
-  },
-};
 const data = [
   { time: '2018-01-01', description: 'description111' },
   { time: '2018-01-02', description: 'description222' },
@@ -70,147 +55,143 @@ const data = [
 ];
 export const SimpleDemo = () => {
   return (
-    <Theme config={view}>
-      <div>
-        <Wrapper>
-          <TimeLine data={data} />
-        </Wrapper>
-        <Wrapper>
-          <p> 调节高度的 简洁样式</p>
-          <br />
-          <TimeLine>
-            <TimeLineItem time="2018-01-01" />
-            <TimeLineItem time="2018-01-02" />
-            <TimeLineItem time="2018-01-03" />
-            <TimeLineItem time="2018-01-04" />
-            <TimeLineItem time="2018-01-05" />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>有详情描述 简洁样式 </p>
-          <br />
-          <TimeLine>
-            <TimeLineItem time="2018-01-01" description={'description111'} />
-            <TimeLineItem time="2018-01-02" description={'description222'} />
-            <TimeLineItem time="2018-01-03" description={'description333'} />
-            <TimeLineItem time="2018-01-04" description={'description444'} />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>左右分散排列 简洁样式</p>
-          <br />
-          <TimeLine mode={'alternate'}>
-            <TimeLineItem time="2018-01-01" />
-            <TimeLineItem time="2018-01-02" />
-            <TimeLineItem time="2018-01-03" />
-            <TimeLineItem time="2018-01-04" />
-            <TimeLineItem time="2018-01-05" />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p> 左右分散排列 有详情描述 简洁样式</p>
-          <br />
-          <TimeLine mode={'alternate'}>
-            <TimeLineItem time="2018-01-01" description={'description111'} />
-            <TimeLineItem time="2018-01-02" description={'description222'} />
-            <TimeLineItem time="2018-01-03" description={'description333'} />
-            <TimeLineItem time="2018-01-04" description={'description444'} />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>不同状态 简洁样式</p>
-          <br />
-          <TimeLine direction={'left'}>
-            <TimeLineItem time="2018-01-01" status={'success'} />
-            <TimeLineItem time="2018-01-02" />
-            <TimeLineItem time="2018-01-03" status={'failed'} />
-            <TimeLineItem time="2018-01-04" />
-            <TimeLineItem time="2018-01-05" />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>自定义元素 图标样式 </p>
-          <br />
-          <TimeLine>
-            <TimeLineItem
-              time="2018-01-01"
-              description={'description111'}
-              type={'icon'}
-              icon={'lugia-icon-financial_progress'}
-            />
-            <TimeLineItem time="2018-01-02" description={'description222'} />
-            <TimeLineItem time="2018-01-03" description={'description333'} />
-            <TimeLineItem
-              time="2018-01-04"
-              description={'description444'}
-              type={'icon'}
-              icon={'lugia-icon-financial_progress'}
-            />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p> 默认 幽灵节点样式 </p>
-          <br />
-          <TimeLine pending={true}>
-            <TimeLineItem time="2018-01-01" description={'description111'} />
-            <TimeLineItem time="2018-01-02" description={'description222'} />
-            <TimeLineItem time="2018-01-03" description={'description333'} />
-            <TimeLineItem time="2018-01-04" description={'description444'} />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>自定义 幽灵节点内容为 Icon </p>
-          <br />
-          <TimeLine pending={true} pendingDot={<Icon iconClass={'lugia-icon-financial_abort'} />}>
-            <TimeLineItem time="2018-01-01" description={'description111'} />
-            <TimeLineItem time="2018-01-02" description={'description222'} />
-            <TimeLineItem time="2018-01-03" description={'description333'} />
-            <TimeLineItem time="2018-01-04" description={'description444'} />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-        <TimeLinePending />
-      </div>
-    </Theme>
+    <div>
+      <Wrapper>
+        <TimeLine data={data} />
+      </Wrapper>
+      <Wrapper>
+        <p> 调节高度的 简洁样式</p>
+        <br />
+        <TimeLine>
+          <TimeLineItem time="2018-01-01" />
+          <TimeLineItem time="2018-01-02" />
+          <TimeLineItem time="2018-01-03" />
+          <TimeLineItem time="2018-01-04" />
+          <TimeLineItem time="2018-01-05" />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>有详情描述 简洁样式 </p>
+        <br />
+        <TimeLine>
+          <TimeLineItem time="2018-01-01" description={'description111'} />
+          <TimeLineItem time="2018-01-02" description={'description222'} />
+          <TimeLineItem time="2018-01-03" description={'description333'} />
+          <TimeLineItem time="2018-01-04" description={'description444'} />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>左右分散排列 简洁样式</p>
+        <br />
+        <TimeLine mode={'alternate'}>
+          <TimeLineItem time="2018-01-01" />
+          <TimeLineItem time="2018-01-02" />
+          <TimeLineItem time="2018-01-03" />
+          <TimeLineItem time="2018-01-04" />
+          <TimeLineItem time="2018-01-05" />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p> 左右分散排列 有详情描述 简洁样式</p>
+        <br />
+        <TimeLine mode={'alternate'}>
+          <TimeLineItem time="2018-01-01" description={'description111'} />
+          <TimeLineItem time="2018-01-02" description={'description222'} />
+          <TimeLineItem time="2018-01-03" description={'description333'} />
+          <TimeLineItem time="2018-01-04" description={'description444'} />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>不同状态 简洁样式</p>
+        <br />
+        <TimeLine direction={'left'}>
+          <TimeLineItem time="2018-01-01" status={'success'} />
+          <TimeLineItem time="2018-01-02" />
+          <TimeLineItem time="2018-01-03" status={'failed'} />
+          <TimeLineItem time="2018-01-04" />
+          <TimeLineItem time="2018-01-05" />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>自定义元素 图标样式 </p>
+        <br />
+        <TimeLine>
+          <TimeLineItem
+            time="2018-01-01"
+            description={'description111'}
+            type={'icon'}
+            icon={'lugia-icon-financial_progress'}
+          />
+          <TimeLineItem time="2018-01-02" description={'description222'} />
+          <TimeLineItem time="2018-01-03" description={'description333'} />
+          <TimeLineItem
+            time="2018-01-04"
+            description={'description444'}
+            type={'icon'}
+            icon={'lugia-icon-financial_progress'}
+          />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p> 默认 幽灵节点样式 </p>
+        <br />
+        <TimeLine pending={true}>
+          <TimeLineItem time="2018-01-01" description={'description111'} />
+          <TimeLineItem time="2018-01-02" description={'description222'} />
+          <TimeLineItem time="2018-01-03" description={'description333'} />
+          <TimeLineItem time="2018-01-04" description={'description444'} />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>自定义 幽灵节点内容为 Icon </p>
+        <br />
+        <TimeLine pending={true} pendingDot={<Icon iconClass={'lugia-icon-financial_abort'} />}>
+          <TimeLineItem time="2018-01-01" description={'description111'} />
+          <TimeLineItem time="2018-01-02" description={'description222'} />
+          <TimeLineItem time="2018-01-03" description={'description333'} />
+          <TimeLineItem time="2018-01-04" description={'description444'} />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+      <TimeLinePending />
+    </div>
   );
 };
 export const OtherDemo = () => {
   return (
-    <Theme config={view}>
-      <div>
-        <Wrapper>
-          <p>节点说明样式 </p>
-          <br />
-          <TimeLine>
-            <TimeLineItem time="2018-01-01" />
-            <TimeLineItem time="2018-01-02" timeLineType="explain" />
-            <TimeLineItem time="2018-01-03" />
-            <TimeLineItem time="2018-01-04" timeLineType="explain" />
-            <TimeLineItem time="2018-01-05" />
-          </TimeLine>
-        </Wrapper>
-        <Wrapper>
-          <p>节点说明样式 </p>
-          <br />
-          <TimeLine>
-            <TimeLineItem time="2018-01-01" description={'description111'} />
-            <TimeLineItem time="2018-01-02" description={'description222'} timeLineType="explain" />
-            <TimeLineItem
-              time="2018-01-03"
-              description={'description333'}
-              timeLineType="icon"
-              icon={'lugia-icon-financial_Hourglass'}
-            />
-            <TimeLineItem time="2018-01-04" description={'description444'} timeLineType="explain" />
-            <TimeLineItem time="2018-01-05" description={'description555'} />
-          </TimeLine>
-        </Wrapper>
-      </div>
-    </Theme>
+    <div>
+      <Wrapper>
+        <p>节点说明样式 </p>
+        <br />
+        <TimeLine>
+          <TimeLineItem time="2018-01-01" />
+          <TimeLineItem time="2018-01-02" timeLineType="explain" />
+          <TimeLineItem time="2018-01-03" />
+          <TimeLineItem time="2018-01-04" timeLineType="explain" />
+          <TimeLineItem time="2018-01-05" />
+        </TimeLine>
+      </Wrapper>
+      <Wrapper>
+        <p>节点说明样式 </p>
+        <br />
+        <TimeLine>
+          <TimeLineItem time="2018-01-01" description={'description111'} />
+          <TimeLineItem time="2018-01-02" description={'description222'} timeLineType="explain" />
+          <TimeLineItem
+            time="2018-01-03"
+            description={'description333'}
+            timeLineType="icon"
+            icon={'lugia-icon-financial_Hourglass'}
+          />
+          <TimeLineItem time="2018-01-04" description={'description444'} timeLineType="explain" />
+          <TimeLineItem time="2018-01-05" description={'description555'} />
+        </TimeLine>
+      </Wrapper>
+    </div>
   );
 };
 export default () => {
