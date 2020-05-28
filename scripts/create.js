455dccbd52e03d9f3b2771a1e15a77ca595355dd
Shine_Lee <wangyi_lixian@163.com>
2020-05-28 10:33:13 +0800

    Refactor: delete useless

--- scripts/create.js
+++ scripts/create.js
@@ -39,9 +39,6 @@ const hideInTollPanelComponents = {
   collapse: 'collapse',
   notification: 'notification',
   popover: 'popover',
-  loading: 'loading',
-  skeleton: 'skeleton',
-  divider: 'divider',
   tooltip: 'index',
   popconfirm: 'popconfirm',
 };
