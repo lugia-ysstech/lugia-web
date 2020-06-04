d954b997388cceac018e387b3ea04d1824e62f03
jelly@pudding <liang_guodong@126.com>
2020-06-04 21:12:08 +0800

    Add    card add  type ='transparent'

--- src/widgets/card/index.js
+++ src/widgets/card/index.js
@@ -487,6 +487,9 @@ class Card extends React.Component<CardProps, CardState> {
     );
     const { operation, title, description, content, children } = this.props;
     const hasChildren = operation || title || description || content || children;
+    if (type === 'transparent') {
+      return <CardOutContainer themeProps={resultTheme} />;
+    }
     return (
       <CardOutContainer themeProps={resultTheme}>
         {hasChildren ? (
