5a752c06070b49599fd589e43b8ba78c956f0b19
cuicuiwang <1623330976@qq.com>
2020-06-10 16:55:39 +0800

    update：trigger 增加 始终展开属性 alwaysOpen

--- src/widgets/trigger/OpenTrigger.js
+++ src/widgets/trigger/OpenTrigger.js
@@ -4,18 +4,14 @@
  * @author cuixia wang
  * @date 2020-06-10
  */
-import React, { Component } from 'react';
+import React from 'react';
 import Trigger from './index';
 type TypeProps = {
   alwaysOpen?: boolean,
   popupVisible?: boolean,
 };
-class OpenTrigger extends Component<TypeProps, any> {
-  render() {
-    const { alwaysOpen } = this.props;
-    const newPopupVisible = alwaysOpen ? { popupVisible: true } : {};
-    return <Trigger {...this.props} {...newPopupVisible} alwaysOpen={alwaysOpen} />;
-  }
+export default function OpenTrigger(props: TypeProps) {
+  const { alwaysOpen } = props;
+  const newPopupVisible = alwaysOpen ? { popupVisible: true } : {};
+  return <Trigger {...props} {...newPopupVisible} alwaysOpen={alwaysOpen} />;
 }
-
-export default OpenTrigger;
