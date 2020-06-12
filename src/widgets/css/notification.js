d9b48d45c62f0d2fdb98abe5f704e7e0e958d8a0
Shine_Lee <wangyi_lixian@163.com>
2020-06-12 10:40:48 +0800

    Fix: change notification public value

--- src/widgets/css/notification.js
+++ src/widgets/css/notification.js
@@ -79,60 +79,67 @@ const getAnimate = (props: CSSProps) => {
     `;
   }
 };
-const getOutBoxShadow = () => {
+const getNotificationCssStyle = () => {
   const { x, y, color, blur, spread } = get('normalBoxShadow');
-  return `${em(x)} ${em(y)} ${em(blur)} ${em(spread)} ${color}`;
+  return `
+    box-shadow:${em(x)} ${em(y)} ${em(blur)} ${em(spread)} ${color};
+    border-radius: ${em(get('largeBorderRadiusValue'))};
+  `;
 };
 export const Notification = styled.div`
   font-size: ${em(14)};
   box-sizing: border-box;
   padding: ${em(20)} ${props => (props.needIcon ? em(44) : em(20))};
-  border-radius: ${em(get('largeBorderRadiusValue'))};
-  box-shadow: ${getOutBoxShadow};
   background: #fff;
   line-height: 1.5;
   position: relative;
   margin-bottom: ${em(16)};
+  ${getNotificationCssStyle}
   ${getAnimate};
 `;
 export const Content = styled.div`
   box-sizing: border-box;
 `;
+const getTitleCssStyle = () =>
+  `font-size: ${em(get('headLineFontSize'))};color: ${get('blackColor')};`;
 export const Title = styled.div`
-  font-size: ${em(16)};
   font-weight: 500;
-  color: ${get('blackColor')};
   margin-bottom: ${em(8)};
   line-height: ${em(24)};
   display: inline-block;
   word-break: break-all;
+  ${getTitleCssStyle}
 `;
+const getTextCssStyle = () =>
+  `font-size: ${em(get('sectionFontSize'))};color: ${get('darkGreyColor')};`;
 export const Text = styled.div`
-  font-size: ${em(14)};
   font-weight: 500;
-  color: ${get('darkGreyColor')};
   word-break: break-all;
+  ${getTextCssStyle}
 `;
+const getCloseIconCssStyle = () => `font-size: ${em(get('xsFontSize'))};`;
 export const CloseIcon: Object = styled(Icon)`
-  font-size: ${em(get('xsFontSize'))};
+  ${getCloseIconCssStyle}
 `;
+const getCloseIconWrapCssStyle = () =>
+  `color: ${get('mediumGreyColor')};:hover { color: ${get('darkGreyColor')}; }`;
 export const CloseIconWrap = styled.div`
   position: absolute;
   right: ${em(16)};
   top: ${em(11.5)};
-  color: ${get('mediumGreyColor')};
   outline: none;
   cursor: pointer;
-  :hover {
-    color: ${get('darkGreyColor')};
-  }
+  ${getCloseIconWrapCssStyle}
+`;
+const getIconsCssStyle = (props: CSSProps) => `font-size: ${em(get('mFontSize'))};color: ${
+  props.iconColor ? get(props.iconColor) : get('themeColor')
+};
 `;
 export const Icons: Object = styled(Icon)`
   position: absolute;
-  font-size: ${em(20)};
   line-height: ${em(22)};
   top: ${em(22)};
   left: ${em(16)};
   cursor: default;
-  color: ${props => (props.iconColor ? get(props.iconColor) : get('themeColor'))};
+  ${getIconsCssStyle}
 `;
