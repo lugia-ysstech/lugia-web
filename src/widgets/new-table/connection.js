da7df3f3a67a453af444dec4cdd7451d083c16a1
SleepyAries <410100498@qq.com>
2020-06-22 19:29:45 +0800

    fix:    修复可编辑表格多级数据无法选中的问题

--- src/widgets/new-table/connection.js
+++ src/widgets/new-table/connection.js
@@ -178,7 +178,7 @@ export default class EditTableEventListener extends Listener<any> {
     return dataKeyMap;
   };
 
-  getSelectColumnMark = (dataIndex: number): number => {
+  getSelectColumnMark = (dataIndex: string): number => {
     return this.dataKeyMap.columnsMap[dataIndex];
   };
 
