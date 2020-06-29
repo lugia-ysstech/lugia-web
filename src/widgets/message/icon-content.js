0a37dca03e0ba5af6ba14aa76d0fa294248e3e7b
Shine_Lee <wangyi_lixian@163.com>
2020-06-29 19:13:49 +0800

    Fix: change logic of icon

--- src/widgets/message/icon-content.js
+++ src/widgets/message/icon-content.js
@@ -55,7 +55,11 @@ export default class extends React.Component<IconConProps, IconConState> {
     const { icon, iconType, content, textTheme, height } = this.props;
     return (
       <MessageTextWrap height={height}>
-        <Icon iconClass={icon || IconInfo[iconType].class} {...this.getIconTheme()} singleTheme />
+        <Icon
+          iconClass={iconType ? IconInfo[iconType].class : icon}
+          {...this.getIconTheme()}
+          singleTheme
+        />
         <MessageText themeProps={textTheme}>{content}</MessageText>
       </MessageTextWrap>
     );
