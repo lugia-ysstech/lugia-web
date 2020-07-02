54ebe6f88de5b4b6a1b45b5f50d8f06a198778c7
cuicuiwang <1623330976@qq.com>
2020-07-02 16:48:28 +0800

    修改：边框设置默认宽度

--- src/widgets/date-picker/themeConfig/themeConfig.js
+++ src/widgets/date-picker/themeConfig/themeConfig.js
@@ -305,6 +305,7 @@ export function getFacePanelContain(props) {
     borderRadius: getBorderRadius(borderRadiusValue),
     boxShadow: normalBoxShadow,
     width: isRange ? 600 : 300,
+    border: getBorder({ width: 0 }),
   };
   const normalTheme = deepMerge(defaultNormal, normal);
   const { width } = normalTheme;
