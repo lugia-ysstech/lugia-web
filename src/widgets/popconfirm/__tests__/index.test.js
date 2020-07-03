121aa2d8bb1ad8e6a7b1728f88c5479675df5d87
jelly@pudding <liang_guodong@126.com>
2020-07-03 17:29:08 +0800

    Update     update Popconfirm  about theme

--- src/widgets/popconfirm/__tests__/index.test.js
+++ src/widgets/popconfirm/__tests__/index.test.js
@@ -10,10 +10,12 @@ import { WrapperDemo } from '../demo';
 import Enzyme, { mount } from 'enzyme';
 import 'jest-styled-components';
 import Adapter from 'enzyme-adapter-react-16';
+import ReactDOM from 'react-dom';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('popconfirm Demo', () => {
   it('Component JSON', () => {
+    ReactDOM.createPortal = node => node;
     global.svtest = true;
     const renders = renderer.create(<WrapperDemo />);
     expect(renders.toJSON()).toMatchSnapshot();
