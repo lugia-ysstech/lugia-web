d5cd8deb727bf9c690591adf93a7f423b936681e
zenjava <138962816@qq.com>
2020-07-04 18:34:39 +0800

    新增组件：   * 用于lugia可视化开发环境中用于放置子页面。

---
+++ src/widgets/lugiad/index.js
@@ -0,0 +1,54 @@
+/**
+ *
+ * create by ligx
+ *
+ * @flow
+ */
+import React from 'react';
+import CSSComponent, { css } from '../theme/CSSProvider';
+import ThemeHoc from '../theme-provider';
+import Widget from '../consts';
+
+const LugiadContainer = CSSComponent({
+  tag: 'div',
+  className: 'CardOutContainer',
+  normal: {
+    selectNames: [
+      ['width'],
+      ['height'],
+      ['background'],
+      ['boxShadow'],
+      ['overflow'],
+      ['border'],
+      ['borderRadius'],
+      ['margin'],
+      ['padding'],
+      ['opacity'],
+    ],
+    defaultTheme: {
+      background: {
+        color: 'white',
+      },
+    },
+  },
+  hover: {
+    selectNames: [['background'], ['border'], ['borderRadius'], ['boxShadow'], ['opacity']],
+  },
+  clicked: {
+    selectNames: [['background'], ['border'], ['borderRadius'], ['boxShadow'], ['opacity']],
+  },
+  css: css`
+    position: relative;
+    display: flex;
+  `,
+  option: { hover: true },
+});
+
+const Lugiad = (props: Object) => {
+  return (
+    <LugiadContainer themeProps={props.getPartOfThemeProps('Container')}>
+      {props.children}
+    </LugiadContainer>
+  );
+};
+export default ThemeHoc(Lugiad, Widget.Lugiad, { hover: true, active: true });
