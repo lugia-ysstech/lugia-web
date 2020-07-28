a9c19f4e894b887ade9f0ae89e852baa55c3ed5a
jelly@pudding <liang_guodong@126.com>
2020-07-28 16:44:54 +0800

    Update     Update  TimeLine theme json

--- src/widgets/css/time-line.js
+++ src/widgets/css/time-line.js
@@ -1,3 +1,3 @@
-export type TimeLineType = 'icon' | 'explain';
+export type TimeLineItemType = 'dot' | 'icon' | 'explain';
 export type TimeLineMode = 'left' | 'right' | 'alternate';
 export type TimeLineStatus = 'success' | 'failed' | 'normal';
