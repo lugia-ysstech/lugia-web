1aea8a9e3d72d5ae3823bb59ba2513179390fd3a
wangcuixia <1623330976@qq.com>
2020-09-08 18:26:21 +0800

    修改：对象深度克隆和比较方法引用lodash 中的 cloneDeep 和 isEqual

--- src/widgets/table/utils.js
+++ src/widgets/table/utils.js
@@ -1,33 +1,8 @@
-import isEqual from 'lodash/isEqual';
-export const deepCopy = (arr: Object[]) => {
-  if (typeof arr !== 'object') return;
+import _ from 'lodash';
 
-  const newArr = arr instanceof Array ? [] : {};
-  for (const key in arr) {
-    if (arr.hasOwnProperty(key)) {
-      newArr[key] = typeof arr[key] === 'object' ? deepCopy(arr[key]) : arr[key];
-    }
-  }
-  return newArr;
+export const deepCopy = (arr: Object[]) => {
+  return _.cloneDeep(arr);
 };
-export const isEqualArray = (arr: any[], newArr: any[], opt: { isStrengthen: false }) => {
-  if (arr.length !== newArr.length) {
-    return false;
-  }
-  const { isStrengthen } = opt;
-  arr = arr.map(item => JSON.stringify(item));
-  newArr = newArr.map(item => JSON.stringify(item));
-  let flag = true;
-  newArr.forEach((item, index) => {
-    if (isStrengthen) {
-      if (arr.indexOf(item) !== index) {
-        flag = false;
-      }
-    } else {
-      if (arr.indexOf(item) <= -1) {
-        flag = false;
-      }
-    }
-  });
-  return flag;
+export const isEqualArray = (arr: any[], newArr: any[]) => {
+  return _.isEqual(arr, newArr);
 };
