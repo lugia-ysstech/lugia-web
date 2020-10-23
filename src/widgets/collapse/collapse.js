dee3aa513600ec1511a8053d919b59468d7bb287
Shine_Lee <wangyi_lixian@163.com>
2020-10-23 11:29:56 +0800

    Add: change log to info

--- src/widgets/collapse/collapse.js
+++ src/widgets/collapse/collapse.js
@@ -28,7 +28,7 @@ export default ThemeProvider(
     static getDerivedStateFromProps(props, state) {
       const { value, activeValue, defaultValue, defaultActiveValue, accordion } = props;
       if (activeValue || defaultActiveValue) {
-        console.log('activeValue和defaultActiveValue即将废弃, 请使用value或defaultValue替代');
+        console.info('activeValue和defaultActiveValue即将废弃, 请使用value或defaultValue替代');
       }
       const hasValue = 'value' in props || 'activeValue' in props;
       const theActiveValue = value || activeValue;
