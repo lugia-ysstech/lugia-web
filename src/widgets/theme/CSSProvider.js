5097d8723d482748cdc04d983c58b748089bf47a
fjianzhou <fanjianzhou@ysstech.com>
2020-11-04 17:35:35 +0800

    修改功能: 修正CSSProvider.test测试用例

--- src/widgets/theme/CSSProvider.js
+++ src/widgets/theme/CSSProvider.js
@@ -10,3 +10,5 @@ export { default } from '@lugia/theme-css-hoc';
 export { StaticComponent } from '@lugia/theme-css-hoc';
 export { css } from '@lugia/theme-css-hoc';
 export { keyframes } from '@lugia/theme-css-hoc';
+export { deepMerge, getAttributeValue, packObject } from '@lugia/object-utils';
+export { style2css } from '@lugia/css';
