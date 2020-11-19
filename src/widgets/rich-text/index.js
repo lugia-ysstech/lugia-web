f7cf775f98c72e21b6b5dee29f832fb55376dfef
Shine_Lee <wangyi_lixian@163.com>
2020-11-19 13:26:04 +0800

    Add: richText label(富文本)

---
+++ src/widgets/rich-text/index.js
@@ -0,0 +1,82 @@
+/**
+ *
+ *
+ * create by shine
+ * @flow
+ */
+import '../common/shirm';
+
+import * as React from 'react';
+import ThemeProvider from '../theme-provider';
+import Widget from '../consts/index';
+import CSSComponent, { css } from '@lugia/theme-css-hoc';
+
+const sectionFontSize = '$lugia-dict.@lugia/lugia-web.sectionFontSize';
+const blackColor = '$lugia-dict.@lugia/lugia-web.blackColor';
+
+const LabelContainer = CSSComponent({
+  tag: 'div',
+  className: 'Container',
+  normal: {
+    selectNames: [
+      ['margin'],
+      ['padding'],
+      ['cursor'],
+      ['width'],
+      ['height'],
+      ['border'],
+      ['borderRadius'],
+      ['boxShadow'],
+      ['background'],
+    ],
+    getThemeMeta() {
+      return {
+        color: blackColor,
+        fontSize: sectionFontSize,
+      };
+    },
+    getCSS: (theme: Object, themeProps: Object) => {
+      const { textAlign } = theme;
+      return `text-align: ${textAlign}`;
+    },
+  },
+  hover: {
+    selectNames: [['cursor'], ['border'], ['borderRadius'], ['boxShadow'], ['background']],
+  },
+  disabled: {
+    selectNames: [['cursor'], ['border'], ['borderRadius'], ['boxShadow'], ['background']],
+  },
+  css: css`
+    display: inline-block;
+    box-sizing: border-box;
+    white-space: pre;
+  `,
+  option: { hover: true },
+});
+
+type RichTextProps = {
+  text?: string,
+  title?: string,
+  themeProps: Object,
+  onClick: Function,
+  getPartOfThemeHocProps: Function,
+  getPartOfThemeProps: Function,
+};
+type RichTextState = {};
+
+class RichText extends React.Component<RichTextProps, RichTextState> {
+  render() {
+    const { text = 'RichText', title = '', onClick = () => {} } = this.props;
+    const themeProps = this.props.getPartOfThemeProps('Container');
+    return (
+      <LabelContainer
+        themeProps={themeProps}
+        onClick={onClick}
+        title={title}
+        dangerouslySetInnerHTML={{ __html: text }}
+      />
+    );
+  }
+}
+
+export default ThemeProvider(RichText, Widget.RichText);
