bb78aeb752a728a40fe3e715354b32e9822f8797
grguang <945455382@qq.com>
2020-12-01 10:18:42 +0800

    新增:     * 工具箱隐藏组件json文件;

--- scripts/build.js
+++ scripts/build.js
@@ -6,18 +6,10 @@
  */
 const compileComponent = require('@lugia/devtools-widgets');
 const inValidComponent = require('../src/widgets/invalidComponent.json');
+const hideInTollPanelComponents = require('../src/widgets/hideInTollPanelComponents.json');
 
 const commonInvalid = ['common', 'consts', 'css', 'utils', 'static'];
 const componentInvalid = [...commonInvalid, ...inValidComponent];
-const hideInTollPanelComponents = {
-  message: 'message',
-  anchor: 'index',
-  notification: 'notification',
-  popover: 'index',
-  tooltip: 'index',
-  popconfirm: 'index',
-  'back-top': 'index',
-};
 const themeInvalid = [...commonInvalid];
 
 const extendCode = "import { load } from './css/theme-common-dict.js';\n" + 'export { load };';
