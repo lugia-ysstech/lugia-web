24327b27cddccfe0a63768e1cf63191c194b69b2
zenjava <138962816@qq.com>
2021-08-18 23:37:54 +0800

    修改：  * 新增demo打包逻辑

---
+++ scripts/package.js
@@ -0,0 +1,39 @@
+const Compile = require("@lugia/lugia-package-demo").default;
+const { join } = require("path");
+const projectPath = join(__dirname, "../");
+const templateToCopyPath = join(__dirname, "../", "createDemo");
+
+// publicPath: 'http://192.168.102.79:9000/component-project/1/'
+
+const demoCompile = new Compile({
+  npmRegistry: "https://registry.npmjs.org/",
+  templateToCopyPath,
+  projectPath,
+  demoProjectOutputPath: "demo-site",
+  entryFile: "createDemo/src/index.js",
+  extraBabelIncludes: [
+    /decamelize/,
+    /debug/,
+    /d3-regression/,
+    /d3-array/,
+    /d3-sankey/,
+    /d3-hierarchy/
+  ],
+  extraBabelPlugins: [
+    [
+      "import",
+      {
+        libraryName: "@lugia/lugia-web",
+        libraryDirectory: "dist"
+      },
+      "@lugia/lugia-web"
+    ]
+  ],
+  alias: {
+    "@": "./src"
+  },
+  isRemoveTemporaryFiles: false
+});
+demoCompile.compile().then(data => {
+  console.log("成功", data);
+});
