86fb7fe3d88504da0b98fbcadbc2cf25b9f4c780
zenjava <138962816@qq.com>
2021-08-31 01:05:11 +0800

    修改：  * 增加自定义对话框弹出位置。

--- src/widgets/modal/demo/UserDefinePositionDemo.js
+++ src/widgets/modal/demo/UserDefinePositionDemo.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Modal from '../index';
 import Widgets from '../../consts/index';
 import Theme from '../../theme';
+import Button from '../../button';
 
 export default () => {
   const [visible, setVisible] = useState(false);
@@ -17,12 +18,15 @@ export default () => {
       },
     },
   };
+  const showModal = () => setVisible(true);
   return (
     <Theme config={modalPositionConfig}>
+      {' '}
+      <Button onClick={showModal}>自定义位置</Button>
       <Modal
         visible={visible}
         title="这是标题！"
-        onOk={() => setVisible(true)}
+        onOk={showModal}
         onCancel={() => setVisible(false)}
       >
         这是内容！
