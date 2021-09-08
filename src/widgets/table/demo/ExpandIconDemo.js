230bc3d4aac061bf5ee81dcd51bf9cc68414e4d9
zhanglunyu <895878230@qq.com>
2021-09-08 19:38:45 +0800

    修改：修改data

--- src/widgets/table/demo/ExpandIconDemo.js
+++ src/widgets/table/demo/ExpandIconDemo.js
@@ -27,46 +27,16 @@ const columns = [
 ];
 
 const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    tel: '0571-22098909',
-    phone: 18889898989,
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    tel: '0571-22098333',
-    phone: 18889898888,
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    tel: '0575-22098909',
-    phone: 18900010002,
-    address: 'Sidney No. 1 Lake Park',
-  },
-  {
-    key: '4',
-    name: 'Jim Red',
-    age: 18,
-    tel: '0575-22098909',
-    phone: 18900010002,
-    address: 'London No. 2 Lake Park',
-  },
-  {
-    key: '5',
-    name: 'Jake White',
-    age: 18,
-    tel: '0575-22098909',
-    phone: 18900010002,
-    address: 'Dublin No. 2 Lake Park',
-  },
+  { name: 'Jack', age: 28, address: 'some where', key: '1' },
+  { name: 'Rose', age: 36, address: 'some where', key: '2' },
+  { name: 'Rook', age: 22, address: 'some where', key: '3' },
+  { name: 'Lise', age: 33, address: 'some where', key: '4' },
+  { name: 'Lise', age: 33, address: 'some where', key: '5' },
+  { name: 'Lise', age: 33, address: 'some where', key: '6' },
+  { name: 'Lise', age: 33, address: 'some where', key: '7' },
+  { name: 'asdasda', age: 33, address: 'some where', key: '8' },
+  { name: 'asdasda', age: 33, address: 'some where', key: '9' },
+  { name: 'asdasda', age: 33, address: 'some where', key: '10' },
 ];
 
 export default class ExpandIconDemo extends React.Component {
