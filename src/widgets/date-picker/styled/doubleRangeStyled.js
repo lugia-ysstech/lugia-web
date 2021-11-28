0921175025e6187944381eed660f6d0ce001fe06
王翠霞 <1623330976@qq.com>
2021-11-28 15:51:24 +0800

    修改：样式调整，属性类型定义

--- src/widgets/date-picker/styled/doubleRangeStyled.js
+++ src/widgets/date-picker/styled/doubleRangeStyled.js
@@ -2,34 +2,12 @@
  * @Author:cuixiawang
  * @Date:
  */
-import CSSComponent from '@lugia/theme-css-hoc';
 import styled from 'styled-components';
 
-export const Wrap = CSSComponent({
-  tag: 'div',
-  className: 'DoubleRangeWrap',
-  normal: {
-    defaultTheme: { width: '100%' },
-    selectNames: [['width'], ['margin']],
-  },
-  hover: {
-    selectNames: [],
-  },
-  focus: {
-    selectNames: [],
-  },
-  active: {
-    selectNames: [],
-  },
-  disabled: {
-    selectNames: [],
-  },
-  css: `
-    display: flex;
-    align-items: center;
-  `,
-});
-
+export const WrapInner = styled.div`
+  display: flex;
+  align-items: center;
+`;
 export const RangeSingle = styled.div`
   flex-grow: 1;
 `;
--- src/widgets/date-picker/triggerPanel/RangePicker_double_input.js
+++ src/widgets/date-picker/triggerPanel/RangePicker_double_input.js
@@ -6,12 +6,13 @@ import React, { useRef } from 'react';
 import { RangeMiddleSpan } from '../styled/styledRangeInput';
 import getThemeProps, { getRangeInputMiddleSymbolTheme } from '../themeConfig/themeConfig';
 import DateInput from './singlePanelTrigger';
-import { Wrap, RangeSingle } from '../styled/doubleRangeStyled';
+import { RangeSingle, WrapInner } from '../styled/doubleRangeStyled';
+import { Box } from '../styled/styled';
 import { getPlaceholder } from '../utils/getDerived';
-
+import { TypeProps } from './rangePanelTrigger';
 const startIndex = 0;
 const endIndex = 1;
-export default props => {
+export default (props: TypeProps) => {
   const {
     middleSymbol = '~',
     disabled,
@@ -37,7 +38,7 @@ export default props => {
   const valueRef = useRef([startVal, endVal]);
   const oldValueRef = useRef([startVal, endVal]);
 
-  const onDateChange = (valueIndex: 0 | 1) => (param: Object) => {
+  const onDateChange = (valueIndex: 0 | 1) => (param: { newValue: string, oldValue: string }) => {
     const { newValue, oldValue } = param;
     const { current: valueRefCurrent } = valueRef;
     const { current: oldValueRefCurrent } = oldValueRef;
@@ -55,30 +56,32 @@ export default props => {
   const newPlaceholder = getPlaceholder({ placeholder, mode });
 
   return (
-    <Wrap themeProps={containerTheme}>
-      <RangeSingle>
-        <DateInput
-          {...props}
-          {...startValue}
-          onChange={onDateChange(startIndex)}
-          mode={'date'}
-          disabled={disabled || disabledStartTime}
-          defaultValue={defaultValue[startIndex]}
-          placeholder={newPlaceholder[startIndex]}
-        />
-      </RangeSingle>
-      <RangeMiddleSpan themeProps={middleSymbolTheme}>{middleSymbol}</RangeMiddleSpan>
-      <RangeSingle>
-        <DateInput
-          {...props}
-          {...endValue}
-          onChange={onDateChange(endIndex)}
-          mode={'date'}
-          disabled={disabled || disabledEndTime}
-          defaultValue={defaultValue[endIndex]}
-          placeholder={newPlaceholder[endIndex]}
-        />
-      </RangeSingle>
-    </Wrap>
+    <Box themeProps={containerTheme}>
+      <WrapInner>
+        <RangeSingle>
+          <DateInput
+            {...props}
+            {...startValue}
+            onChange={onDateChange(startIndex)}
+            mode={'date'}
+            disabled={disabled || disabledStartTime}
+            defaultValue={defaultValue[startIndex]}
+            placeholder={newPlaceholder[startIndex]}
+          />
+        </RangeSingle>
+        <RangeMiddleSpan themeProps={middleSymbolTheme}>{middleSymbol}</RangeMiddleSpan>
+        <RangeSingle>
+          <DateInput
+            {...props}
+            {...endValue}
+            onChange={onDateChange(endIndex)}
+            mode={'date'}
+            disabled={disabled || disabledEndTime}
+            defaultValue={defaultValue[endIndex]}
+            placeholder={newPlaceholder[endIndex]}
+          />
+        </RangeSingle>
+      </WrapInner>
+    </Box>
   );
 };
