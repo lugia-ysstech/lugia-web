73e1c09237f313cb2159a63dffd8ba109585596f
zenjava <138962816@qq.com>
2021-12-07 22:55:05 +0800

    修正bug:  * 空值应该是一个字符串数组。

--- src/widgets/date-picker/triggerPanel/RangePickerDoubleInput.js
+++ src/widgets/date-picker/triggerPanel/RangePickerDoubleInput.js
@@ -64,7 +64,7 @@ export default (props: Props) => {
 
   useEffect(() => {
     if (valueRef.current) {
-      valueRef.current = value ? [...value] : [];
+      valueRef.current = value ? [...value] : ['', ''];
     }
   }, [value]);
 
