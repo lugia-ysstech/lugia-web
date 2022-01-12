c4efd532c005f0425ab5dddaeddac5f7d7fe3d86
shine_lee <wangyi_lixian@163.com>
2022-01-12 17:13:27 +0800

    添加: 初步添加虚拟化表格

---
+++ src/widgets/table/VirtualTable.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { VariableSizeGrid as Grid } from 'react-window';
+import ResizeObserver from 'rc-resize-observer';
+import RcTable from '@lugia/rc-table';
+import classNames from 'classnames';
+
+export default function VirtualTable(props) {
+  const { columns, scroll } = props;
+
+  const [tableWidth, setTableWidth] = useState(0);
+
+  const widthColumnCount = columns.filter(({ width }) => !width).length;
+  const mergedColumns = columns.map(column => {
+    if (column.width) {
+      return column;
+    }
+
+    return { ...column, width: Math.floor(tableWidth / widthColumnCount) };
+  });
+  const gridRef = useRef();
+  const [connectObject] = useState(() => {
+    const obj = {};
+    Object.defineProperty(obj, 'scrollLeft', {
+      get: () => null,
+      set: scrollLeft => {
+        if (gridRef.current) {
+          gridRef.current.scrollTo({
+            scrollLeft,
+          });
+        }
+      },
+    });
+    return obj;
+  });
+
+  const resetVirtualGrid = () => {
+    gridRef.current.resetAfterIndices({
+      columnIndex: 0,
+      shouldForceUpdate: true,
+    });
+  };
+
+  useEffect(() => resetVirtualGrid, [tableWidth]);
+
+  const renderVirtualList = (rawData, { scrollbarSize, ref, onScroll }) => {
+    ref.current = connectObject;
+    const totalHeight = rawData.length * 54;
+    return (
+      <Grid
+        ref={gridRef}
+        className="virtual-grid"
+        columnCount={mergedColumns.length}
+        columnWidth={index => {
+          const { width } = mergedColumns[index];
+
+          return totalHeight > scroll.y && index === mergedColumns.length - 1
+            ? width - scrollbarSize - 1
+            : width;
+        }}
+        height={scroll.y}
+        rowCount={rawData.length}
+        rowHeight={() => 54}
+        width={tableWidth}
+        onScroll={({ scrollLeft }) => {
+          onScroll({
+            scrollLeft,
+          });
+        }}
+      >
+        {({ columnIndex, rowIndex, style }) => (
+          <div
+            className={classNames('virtual-table-cell', {
+              'virtual-table-cell-last': columnIndex === mergedColumns.length - 1,
+            })}
+            style={style}
+          >
+            {rawData[rowIndex][mergedColumns[columnIndex].dataIndex]}
+          </div>
+        )}
+      </Grid>
+    );
+  };
+
+  console.log('render_____inner');
+
+  return (
+    <ResizeObserver
+      onResize={({ width }) => {
+        setTableWidth(width);
+      }}
+    >
+      <RcTable
+        {...props}
+        className="virtual-table"
+        columns={mergedColumns}
+        pagination={false}
+        components={{
+          body: renderVirtualList,
+        }}
+      />
+    </ResizeObserver>
+  );
+}
